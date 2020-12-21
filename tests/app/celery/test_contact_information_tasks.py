b4f22d717330a3095a9d394aa0a8386460dbb0e3
emilywu-tw <ewu@thoughtworks.com>
2020-12-21 16:18:17 -0500

    [#194] Create test for get phone number and update email test
    Co-authored-by: Saman Moshafi <Saman.moshafi@thoughtworks.com>
    Co-authored-by: Filip Fafara <ffafara@thoughtworks.com>
    Co-authored-by: Phil Herbert <pherbert@thoughtworks.com>

--- tests/app/celery/test_contact_information_tasks.py
+++ tests/app/celery/test_contact_information_tasks.py
@@ -4,8 +4,8 @@ import pytest
 
 from app.celery.contact_information_tasks import lookup_contact_info
 from app.exceptions import NotificationTechnicalFailureException
-from app.models import Notification, RecipientIdentifier, Service, NOTIFICATION_TECHNICAL_FAILURE, \
-    NOTIFICATION_PERMANENT_FAILURE
+from app.models import Notification, RecipientIdentifier, NOTIFICATION_TECHNICAL_FAILURE, \
+    NOTIFICATION_PERMANENT_FAILURE, EMAIL_TYPE, SMS_TYPE
 from app.va import IdentifierType
 from app.va.va_profile import VAProfileClient, VAProfileNonRetryableException, \
     VAProfileRetryableException, NoContactInfoException
@@ -29,7 +29,8 @@ def notification():
     return notification
 
 
-def test_should_fetch_notification(client, mocker, notification):
+def test_should_get_email_address_and_update_notification(client, mocker, notification):
+    notification.notification_type = EMAIL_TYPE
     mocked_get_notification_by_id = mocker.patch(
         'app.celery.contact_information_tasks.get_notification_by_id',
         return_value=notification
@@ -46,16 +47,38 @@ def test_should_fetch_notification(client, mocker, notification):
         'app.celery.contact_information_tasks.dao_update_notification'
     )
 
-    mocked_service = mocker.Mock(Service)
-    mocked_service.research_mode = False
-    notification.service = mocked_service
-
     lookup_contact_info(notification.id)
 
     mocked_get_notification_by_id.assert_called()
     mocked_va_profile_client.get_email.assert_called_with(EXAMPLE_VA_PROFILE_ID)
     mocked_update_notification.assert_called_with(notification)
     assert notification.to == 'test@test.org'
+
+
+def test_should_get_phone_number_and_update_notification(client, mocker, notification):
+    notification.notification_type = SMS_TYPE
+    mocked_get_notification_by_id = mocker.patch(
+        'app.celery.contact_information_tasks.get_notification_by_id',
+        return_value=notification
+    )
+
+    mocked_va_profile_client = mocker.Mock(VAProfileClient)
+    mocked_va_profile_client.get_telephone = mocker.Mock(return_value='+15555555555')
+    mocker.patch(
+        'app.celery.contact_information_tasks.va_profile_client',
+        new=mocked_va_profile_client
+    )
+
+    mocked_update_notification = mocker.patch(
+        'app.celery.contact_information_tasks.dao_update_notification'
+    )
+
+    lookup_contact_info(notification.id)
+
+    mocked_get_notification_by_id.assert_called()
+    mocked_va_profile_client.get_telephone.assert_called_with(EXAMPLE_VA_PROFILE_ID)
+    mocked_update_notification.assert_called_with(notification)
+    assert notification.to == '+15555555555'
 
 
 def test_should_not_retry_on_non_retryable_exception(client, mocker, notification):
