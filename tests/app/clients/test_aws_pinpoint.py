d244f043874bcfc227596626c6673721d3b33cce
emilywu-tw <ewu@thoughtworks.com>
2020-12-22 13:41:10 -0500

    [#251] Move duplicated test vars to constants
    Co-authored-by: Ling Tran<ltran@thoughtworks.com>

--- tests/app/clients/test_aws_pinpoint.py
+++ tests/app/clients/test_aws_pinpoint.py
@@ -4,6 +4,13 @@ import botocore
 from app.clients.sms.aws_pinpoint import AwsPinpointClient, AwsPinpointException
 
 
+TEST_CONTENT = "test content"
+TEST_ID = 'some-app-id'
+TEST_MESSAGE_ID = 'message-id'
+TEST_RECIPIENT_NUMBER = "+100000000"
+TEST_REFERENCE = 'test notification id'
+
+
 @pytest.fixture(scope='function')
 def aws_pinpoint_client(notify_api, mocker):
     with notify_api.app_context():
@@ -11,7 +18,7 @@ def aws_pinpoint_client(notify_api, mocker):
         statsd_client = mocker.Mock()
         logger = mocker.Mock()
         aws_pinpoint_client.init_app(
-            aws_pinpoint_app_id='some-app-id',
+            aws_pinpoint_app_id=TEST_ID,
             aws_region='some-aws-region',
             logger=logger,
             origination_number='+10000000000',
@@ -27,79 +34,66 @@ def boto_mock(aws_pinpoint_client, mocker):
 
 
 def test_send_sms_successful_returns_aws_pinpoint_response_messageid(aws_pinpoint_client, boto_mock):
-    test_id = 'some_id'
-    test_recipient_number = "+100000000"
-    test_content = "test content"
-    test_reference = 'test notification id'
-    test_message_id = 'message-id'
-
     boto_mock.send_messages.return_value = {
         'MessageResponse': {
-            'ApplicationId': test_id,
+            'ApplicationId': TEST_ID,
             'RequestId': 'request-id',
             'Result': {
-                test_recipient_number: {
+                TEST_RECIPIENT_NUMBER: {
                     'DeliveryStatus': 'SUCCESSFUL',
-                    'MessageId': test_message_id,
+                    'MessageId': TEST_MESSAGE_ID,
                     'StatusCode': 200,
-                    'StatusMessage': f"MessageId: {test_message_id}",
+                    'StatusMessage': f"MessageId: {TEST_MESSAGE_ID}",
                 }
             }
         }
     }
 
-    response = aws_pinpoint_client.send_sms(test_recipient_number, test_content, test_reference)
+    response = aws_pinpoint_client.send_sms(TEST_RECIPIENT_NUMBER, TEST_CONTENT, TEST_REFERENCE)
 
-    assert response == test_message_id
+    assert response == TEST_MESSAGE_ID
 
 
 def test_send_sms_with_service_sender_number(aws_pinpoint_client, boto_mock, mocker):
-    test_id = aws_pinpoint_client.aws_pinpoint_app_id
-    test_recipient_number = "+100000000"
-    test_content = "test content"
-    test_reference = 'test notification id'
-    test_message_id = 'message-id'
     test_sender = "+12222222222"
 
     boto_mock.send_messages.return_value = {
         'MessageResponse': {
-            'ApplicationId': test_id,
+            'ApplicationId': TEST_ID,
             'RequestId': 'request-id',
             'Result': {
-                test_recipient_number: {
+                TEST_RECIPIENT_NUMBER: {
                     'DeliveryStatus': 'SUCCESSFUL',
-                    'MessageId': test_message_id,
+                    'MessageId': TEST_MESSAGE_ID,
                     'StatusCode': 200,
-                    'StatusMessage': f"MessageId: {test_message_id}",
+                    'StatusMessage': f"MessageId: {TEST_MESSAGE_ID}",
                 }
             }
         }
     }
 
-    aws_pinpoint_client.send_sms(test_recipient_number, test_content, test_reference, sender=test_sender)
+    aws_pinpoint_client.send_sms(TEST_RECIPIENT_NUMBER, TEST_CONTENT, TEST_REFERENCE, sender=test_sender)
 
     message_request_payload = {
         "Addresses": {
-            test_recipient_number: {
+            TEST_RECIPIENT_NUMBER: {
                 "ChannelType": "SMS"
             }
         },
         "MessageConfiguration": {
             "SMSMessage": {
-                "Body": test_content,
+                "Body": TEST_CONTENT,
                 "MessageType": "TRANSACTIONAL",
                 "OriginationNumber": test_sender
             }
         }
     }
 
-    assert boto_mock.send_messages.assert_called_with(ApplicationId=test_id, MessageRequest=message_request_payload)
+    assert boto_mock.send_messages.assert_called_with(ApplicationId=TEST_ID, MessageRequest=message_request_payload)
 
 
 def test_send_sms_throws_aws_pinpoint_exception(aws_pinpoint_client, boto_mock):
-    test_recipient_number = "+1000"
-    test_content = "test content"
-    test_reference = 'test notification id'
+    invalid_recipient_number = "+1000"
 
     error_response = {
         'Error': {
@@ -114,7 +108,7 @@ def test_send_sms_throws_aws_pinpoint_exception(aws_pinpoint_client, boto_mock):
     boto_mock.send_messages.side_effect = botocore.exceptions.ClientError(error_response, 'exception')
 
     with pytest.raises(AwsPinpointException) as exception:
-        aws_pinpoint_client.send_sms(test_recipient_number, test_content, test_reference)
+        aws_pinpoint_client.send_sms(invalid_recipient_number, TEST_CONTENT, TEST_REFERENCE)
 
     assert f"BadRequestException" in str(exception.value)
     aws_pinpoint_client.statsd_client.incr.assert_called_with("clients.sms.error")
