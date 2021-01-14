2cfb54b18f936bab600a95b688fedfae2e8b2d17
Emily Wu <ewu@thoughtworks.com>
2021-01-14 16:49:10 -0500

    [#321] Refactor validate_providers to move raise InvalidRequest into a method to reduce duplication
    Signed-off-by: Saman Moshafi <saman.moshafi@thoughtworks.com>

--- app/provider_details/validate_providers.py
+++ app/provider_details/validate_providers.py
@@ -6,14 +6,13 @@ from app.models import EMAIL_TYPE, SMS_TYPE
 
 
 def validate_service_providers(update_request: dict):
-    # validate any provider that the service points to
     email_provider_id = update_request.get('email_provider_id')
     if not(email_provider_id is None or is_provider_valid(email_provider_id, EMAIL_TYPE)):
-        raise InvalidRequest('invalid email_provider_id', status_code=400)
+        throw_invalid_request_error('email')
 
     sms_provider_id = update_request.get('sms_provider_id')
     if not(sms_provider_id is None or is_provider_valid(sms_provider_id, SMS_TYPE)):
-        raise InvalidRequest('invalid sms_provider_id', status_code=400)
+        throw_invalid_request_error('sms')
 
 
 def is_provider_valid(provider_id: uuid, notification_type: str) -> bool:
@@ -30,4 +29,8 @@ def validate_template_providers(request: dict):
     template_type = request.get('template_type')
 
     if not(provider_id is None or is_provider_valid(provider_id, template_type)):
-        raise InvalidRequest(f'invalid {template_type}_provider_id', status_code=400)
+        throw_invalid_request_error(template_type)
+
+
+def throw_invalid_request_error(template_type):
+    raise InvalidRequest(f'invalid {template_type}_provider_id', status_code=400)
