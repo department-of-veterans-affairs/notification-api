0eb26ad91c4c0194c39d89f2b45362146e836b3b
Vikram Kohli <vikram.kohli@thoughtworks.com>
2021-05-07 16:07:18 -0400

    [#468] Update remaining tests in service callback api dao
    Update create service callback API in DB to pass test
    
    Co-authored-by: Saman Moshafi <saman.moshafi@thoughtworks.com>

--- tests/app/db.py
+++ tests/app/db.py
@@ -61,7 +61,7 @@ from app.models import (
     LetterBranding,
     Domain,
     NotificationHistory,
-    RecipientIdentifier
+    RecipientIdentifier, NOTIFICATION_FAILED
 )
 
 
@@ -483,11 +483,13 @@ def create_service_callback_api(  # nosec
         bearer_token="some_super_secret",
         callback_type="delivery_status"
 ):
+    notification_statuses = [NOTIFICATION_FAILED]
     service_callback_api = ServiceCallbackApi(service_id=service.id,
                                               url=url,
                                               bearer_token=bearer_token,
                                               updated_by_id=service.users[0].id,
-                                              callback_type=callback_type
+                                              callback_type=callback_type,
+                                              notification_statuses=notification_statuses
                                               )
     save_service_callback_api(service_callback_api)
     return service_callback_api
