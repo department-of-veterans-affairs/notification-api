1e9399f8d48653ab80c799f7a33182969fcd45b0
saman.moshafi <saman.moshafi@thoughtworks.com>
2021-05-13 22:23:12 -0400

    [department-of-veterans-affairs/notification-api#468] remove redundant code
    Co-authored-by: Vikram Kohli <vikram.kohli@thoughtworks.com>

--- app/dao/service_callback_api_dao.py
+++ app/dao/service_callback_api_dao.py
@@ -39,12 +39,6 @@ def get_service_callback_api(service_callback_api_id, service_id):
     return ServiceCallbackApi.query.filter_by(id=service_callback_api_id, service_id=service_id).first()
 
 
-# def get_service_delivery_status_callback_api_for_service(service_id):
-#     return ServiceCallbackApi.query.filter_by(
-#         service_id=service_id,
-#         callback_type=DELIVERY_STATUS_CALLBACK_TYPE
-#     ).first()
-
 def get_service_delivery_status_callback_api_for_service(service_id, notification_status):
     return db.session.query(ServiceCallbackApi).filter(
         ServiceCallbackApi.notification_statuses.contains([notification_status]),
