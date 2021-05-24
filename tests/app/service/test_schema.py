2014c0be5524b7351207ab94c96ced70e1380feb
saman.moshafi <saman.moshafi@thoughtworks.com>
2021-05-24 09:22:06 -0400

    [department-of-veterans-affairs/notification-api#468] add the tests back as uuid.uuid4() was the culprit, since different values would have been generated on each gw

--- tests/app/service/test_schema.py
+++ tests/app/service/test_schema.py
@@ -59,6 +59,51 @@ def test_create_service_callback_api_schema_validate_succeeds():
     assert validated == under_test
 
 
+@pytest.mark.parametrize('key, value', [
+    ("url", "https://some_url.for_service"),
+    ("bearer_token", "something_ten_chars"),
+    ("notification_statuses", ["failed"]),
+    ("updated_by_id", "6ce466d0-fd6a-11e5-82f5-e0accb9d11a6"),
+    (None, None)
+])
+def test_create_service_callback_api_schema_validate_fails_when_missing_properties(key, value):
+    under_test = {key: value}
+
+    with pytest.raises(ValidationError) as e:
+        validate(under_test, create_service_callback_api_request_schema)
+
+    errors = json.loads(str(e.value)).get('errors')
+    assert len(errors) > 1
+    for message in errors:
+        assert message['error'] == 'ValidationError'
+        assert 'is a required property' in message['message']
+
+
+@pytest.mark.parametrize('key, wrong_key, value', [
+    ("url", "urls", "https://some_url.for_service"),
+    ("bearer_token", "bearers_token", "something_ten_chars"),
+    ("notification_statuses", "notifications_statuses", ["failed"]),
+    ("updated_by_id", "update_by_id", "6ce466d0-fd6a-11e5-82f5-e0accb9d11a6")
+])
+def test_create_service_callback_api_schema_validate_fails_with_misspelled_keys(key, wrong_key, value):
+    under_test = {
+        "url": "https://some_url.for_service",
+        "bearer_token": "something_ten_chars",
+        "notification_statuses": ["failed"],
+        "updated_by_id": str(uuid.uuid4())
+    }
+    del under_test[key]
+    under_test[wrong_key] = value
+
+    with pytest.raises(ValidationError) as e:
+        validate(under_test, create_service_callback_api_request_schema)
+
+    errors = json.loads(str(e.value)).get('errors')
+    assert len(errors) == 1
+    assert errors[0]['error'] == 'ValidationError'
+    assert errors[0]['message'] == f"{key} is a required property"
+
+
 def test_update_service_callback_api_schema_validate_succeeds():
     under_test = {
         "url": "https://some_url.for_service",
