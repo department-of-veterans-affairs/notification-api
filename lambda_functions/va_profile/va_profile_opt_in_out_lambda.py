002fa044858abafebf12d143db6acabb3d0ab3d9
David J. Kalbfleisch <1.21e9W@protonmail.com>
2022-05-19 19:49:52 -0400

    Implement PUT requests for VA Profile when processing opt in/out preference updates.  Closes #679.

--- lambda_functions/va_profile/va_profile_opt_in_out_lambda.py
+++ lambda_functions/va_profile/va_profile_opt_in_out_lambda.py
@@ -6,13 +6,22 @@ import logging
 import os
 import psycopg2
 import sys
+from http.client import ConnectionError, HTTPSConnection
+from json import dumps
+
 
 OPT_IN_OUT_QUERY = """SELECT va_profile_opt_in_out(%s, %s, %s, %s, %s);"""
 NOTIFICATION_API_DB_URI = os.getenv("notification_api_db_uri")
+VA_PROFILE_DOMAIN = os.getenv("va_profile_domain")
+VA_PROFILE_PATH_BASE = "/communication-hub/communication/v1/status/changelog/"
+
 
 if NOTIFICATION_API_DB_URI is None:
     logging.error("The database URI is not set.")
     sys.exit("Couldn't connect to the database.")
+
+if VA_PROFILE_DOMAIN is None:
+    logging.error("Could not get the domain for VA Profile.")
 
 
 def make_connection():
@@ -47,13 +56,13 @@ def va_profile_opt_in_out_lambda_handler(event: dict, context) -> dict:
             ...
             "bios": [
                 {
-                "txAuditId": "string",
-                "sourceDate": "2022-03-07T19:37:59.320Z",
-                "vaProfileId": 0,
-                "communicationChannelId": 0,
-                "communicationItemId": 0,
-                "allowed": true,
-                ...
+                    "txAuditId": "string",
+                    "sourceDate": "2022-03-07T19:37:59.320Z",
+                    "vaProfileId": 0,
+                    "communicationChannelId": 0,
+                    "communicationItemId": 0,
+                    "allowed": true,
+                    ...
                 }
             ]
         }
@@ -64,14 +73,33 @@ def va_profile_opt_in_out_lambda_handler(event: dict, context) -> dict:
         https://docs.aws.amazon.com/lambda/latest/dg/gettingstarted-concepts.html#gettingstarted-concepts-event
     """
 
+    logging.debug(event)
+
     if "txAuditId" not in event or "bios" not in event or not isinstance(event["bios"], list):
-        # A required top level attribute is missing from the request.
-        logging.debug(event)
-        return { "statusCode": 400 }
+        return { "statusCode": 400, "body": "A required top level attribute is missing from the request or has the wrong type." }
 
     response = { "statusCode": 200 }
 
+    put_request_body = {
+        "txAuditId": event["txAuditId"],
+        "bios": [],
+    }
+
+    # Process the preference updates.
     for record in event["bios"]:
+        put_record = {
+            "vaProfileId": record.get("vaProfileId", "unknown"),
+            "communicationChannelId": record.get("communicationChannelId", "unknown"),
+            "communicationItemId": record.get("communicationItemId", "unknown"),
+        }
+
+        if record.get("txAuditId", '') != event["txAuditId"]:
+            # Do not query the database in response to this record.
+            put_record["status"] = "COMPLETED_FAILURE"
+            put_record["info"] = "The record's txAuditId, {}, does not match the event's txAuditId, {}.".format(record.get('txAuditId', '<unknown>'), event["txAuditId"])
+            put_request_body["bios"].append(put_record)
+            continue
+
         try:
             params = (                             # Stored function parameters:
                 record["VaProfileId"],             #     _va_profile_id
@@ -88,26 +116,40 @@ def va_profile_opt_in_out_lambda_handler(event: dict, context) -> dict:
             if db_connection is None:
                 raise RuntimeError("No database connection.")
 
-            # Execute the appropriate stored function.
-            with connection.cursor() as c:
-                c.execute(OPT_IN_OUT_QUERY, params)
+            # Execute the stored function.
+            with db_connection.cursor() as c:
+                put_record["status"] = "COMPLETED_SUCCESS" if c.execute(OPT_IN_OUT_QUERY, params) else "COMPLETED_NOOP"
         except KeyError as e:
-            # Bad Request
+            # Bad Request.  Required attributes are missing.
             response["statusCode"] = 400
+            put_record["status"] = "COMPLETED_FAILURE"
             logging.exception(e)
-
-            # TODO - set PUT response value
         except Exception as e:
             # Internal Server Error.  Prefer to return 400 if multiple records raise exceptions.
             if response["statusCode"] != 400:
                 response["statusCode"] = 500
+            put_record["status"] = "COMPLETED_FAILURE"
             logging.exception(e)
+        finally:
+            put_request_body["bios"].append(put_record)
 
-            # TODO - set PUT response value
-
-    if response["statusCode"] != 200:
-        logging.debug(event)
-
-    # TODO - PUT back to VA Profile
+    if len(put_request_body["bios"]) > 0 and VA_PROFILE_DOMAIN is not None:
+        try:
+            # Make a PUT request to VA Profile.
+            https_connection = HTTPSConnection(VA_PROFILE_DOMAIN)
+            https_connection.request(
+                "PUT",
+                VA_PROFILE_PATH_BASE + event["txAuditId"],
+                dumps(put_request_body),
+                { "Content-Type": "application/json" }
+            )
+            put_response = https_connection.response()
+            if put_response.status != 200:
+                logging.info("VA Profile responded with HTTP status %d.", put_response.status)
+        except ConnectionError as e:
+            logging.error("The PUT request to VA Profile failed.")
+            logging.exception(e)
+        finally:
+            https_connection.close()
 
     return response
