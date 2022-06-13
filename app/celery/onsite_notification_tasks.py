7ad69c4c2aa080018de31f0a76789d55668b380e
Evan Parish <evan.parish@oddball.io>
2022-06-13 16:08:55 -0400

    Fixing errors with the run.

--- app/celery/onsite_notification_tasks.py
+++ app/celery/onsite_notification_tasks.py
@@ -4,12 +4,12 @@ from app import va_onsite_client
 
 
 @notify_celery.task(name="send-va-onsite-notification-task")
-def send_va_onsite_notification_task(va_profile_id: str, template_id, onsite_enabled):
+def send_va_onsite_notification_task(va_profile_id: str, template_id, onsite_enabled: bool = False):
 
     current_app.logger.info(f'Calling va_onsite_notification_task with va_profile_id: {va_profile_id} |\
                             Template onsite_notification set to: {onsite_enabled}')
 
-    if onsite_enabled and va_profile_id:
+    if onsite_enabled and va_profile_id is not None:
         data = {"template_id": template_id, "va_profile_id": va_profile_id}
         response = va_onsite_client.post_onsite_notification(data)
 
