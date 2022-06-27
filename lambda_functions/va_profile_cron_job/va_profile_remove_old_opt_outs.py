a4e0db43d0009860e79bc9d8c0b8959b9a51efea
maham360 <maham@docme360.com>
2022-06-27 17:49:25 -0400

    squash

--- lambda_functions/va_profile_cron_job/va_profile_remove_old_opt_outs.py
+++ lambda_functions/va_profile_cron_job/va_profile_remove_old_opt_outs.py
@@ -10,42 +10,23 @@ if SQLALCHEMY_DATABASE_URI is None:
     logging.error("The database URI is not set.")
     sys.exit("Couldn't connect to the database.")
 
-db_connection = None
 
-
-def make_connection(worker_id):
+def va_profile_remove_old_opt_outs_handler(event, context, worker_id=None):
     """
-    Return a connection to the database, or return None.
+    This function deletes any va_profile cache records that
+    are opted out and greater than 24 hours old.
     """
-
-    connection = None
 
     # https://www.psycopg.org/docs/module.html#exceptions
     try:
         connection = psycopg2.connect(SQLALCHEMY_DATABASE_URI + ('' if worker_id is None else f"_{worker_id}"))
+        with connection.cursor() as c:
+            c.execute(REMOVE_OPTED_OUT_RECORDS_QUERY)
+            connection.commit()
     except psycopg2.Warning as e:
         logging.warning(e)
     except psycopg2.Error as e:
         logging.exception(e)
         logging.error(e.pgcode)
-
-    return connection
-
-
-def va_profile_remove_old_opt_outs_handler(event, context, worker_id=None):
-    """This function deletes any va_profile cache records that
-    are opted out and greater than 24 hours old."""
-
-    if db_connection is None or db_connection.status != 0:
-        # Attempt to (re-)establish a database connection
-        db_connection = make_connection(worker_id)
-
-    if db_connection is None:
-        raise RuntimeError("No database connection.")
-
-    try:
-        with db_connection.cursor() as c:
-            c.execute(REMOVE_OPTED_OUT_RECORDS_QUERY)
-            db_connection.commit()
     except Exception as e:
         logging.exception(e)
