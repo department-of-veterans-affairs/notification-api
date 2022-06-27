e624673adfb547ff13175bab2250afd950bd1f50
maham360 <maham@docme360.com>
2022-06-27 17:49:25 -0400

    squash

--- tests/lambda_functions/va_profile/test_va_profile_remove_old_opt_outs.py
+++ tests/lambda_functions/va_profile/test_va_profile_remove_old_opt_outs.py
@@ -12,6 +12,11 @@ REMOVE_OPTED_OUT_RECORDS_QUERY = text("""SELECT va_profile_remove_old_opt_outs()
 
 COUNT = r"""SELECT COUNT(*) FROM va_profile_local_cache;"""
 
+COUNT_OPTED_OUT_RECORD_QUERY = text("""SELECT COUNT(*)
+FROM va_profile_local_cache
+WHERE allowed = False
+AND age(NOW(), source_datetime) > INTERVAL '24 hours';""")
+
 
 def setup_db(connection):
     """
@@ -28,12 +33,7 @@ def setup_db(connection):
     count_queryset = connection.execute(COUNT)
     assert count_queryset.fetchone()[0] == 0, "The cache should be empty at the start."
 
-
-def insert_values_into_database(connection):
-    """Inserts values into database for testing."""
-
     expired_datetime = (datetime.now() - timedelta(days=2)).strftime('%Y-%m-%dT%H:%M:%S%z')
-    active_datetime = (datetime.now() + timedelta(days=1)).strftime('%Y-%m-%dT%H:%M:%S%z')
 
     insert_expired_record_opt_out = INSERT_OPT_IN_OUT_RECORD.bindparams(
         va_profile_id=0,
@@ -60,7 +60,7 @@ def insert_values_into_database(connection):
         communication_item_id=2,
         communication_channel_id=3,
         allowed=False,
-        source_datetime=active_datetime
+        source_datetime=datetime.now().strftime('%Y-%m-%dT%H:%M:%S%z')
     )
 
     connection.execute(insert_active_record_opt_out)
@@ -70,16 +70,33 @@ def insert_values_into_database(connection):
         "There should only be three records in the database."
 
 
-def test_remove_opted_out_records_query(notify_db_session):
-    """If the difference between the current time and source_datetime
-    is greater than 24 hours, the stored function should delete the records."""
-    
+def test_count_opted_out_records_query(notify_db_session):
+    """
+    If the value is opted out and the source_datetime
+    is older than 24 hours, it should be selected.
+    """
+
     with notify_db_session.engine.begin() as connection:
         setup_db(connection)
-        insert_values_into_database(connection)
+
+        connection.execute(COUNT_OPTED_OUT_RECORD_QUERY)
+
+        count_opted_out_record = connection.execute(COUNT_OPTED_OUT_RECORD_QUERY)
+        assert count_opted_out_record.fetchone()[0] == 1, \
+            "The stored function should have removed one record that is opted out."
+
+
+def test_remove_opted_out_records_query(notify_db_session):
+    """
+    If the difference between the current time and source_datetime
+    is greater than 24 hours, the stored function should delete the records.
+    """
+
+    with notify_db_session.engine.begin() as connection:
+        setup_db(connection)
 
         connection.execute(REMOVE_OPTED_OUT_RECORDS_QUERY)
 
         count_queryset = connection.execute(COUNT)
         assert count_queryset.fetchone()[0] == 2, \
-            "The stored function should have removed only one record."
+            "The stored function should have two records remaining that are opted in."
