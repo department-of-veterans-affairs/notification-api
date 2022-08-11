4b68a91ea80e0c81fb266445b84e02fab4997fc8
Kyle MacMillan <kyle.macmillan@oddball.io>
2022-08-11 12:31:42 -0500

    Added retry connection

--- lambda_functions/va_profile_remove_old_opt_outs/va_profile_remove_old_opt_outs_lambda.py
+++ lambda_functions/va_profile_remove_old_opt_outs/va_profile_remove_old_opt_outs_lambda.py
@@ -9,6 +9,7 @@ import sys
 REMOVE_OPTED_OUT_RECORDS_QUERY = """SELECT va_profile_remove_old_opt_outs();"""
 SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
 
+
 # Set logger
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
@@ -19,25 +20,9 @@ if SQLALCHEMY_DATABASE_URI is None:
 else:
     logger.info('Execution environment prepared...')
 
+connection = None
 
-def va_profile_remove_old_opt_outs_handler(event=None, context=None, worker_id=None):
-    """
-    This function deletes any va_profile cache records that
-    are opted out and greater than 24 hours old.
-    """
-
-    # https://www.psycopg.org/docs/module.html#exceptions
-    try:
-        connection = make_connection(worker_id)
-        execute_remove_function(connection)
-    except Exception as e:
-        logger.exception(e)
-    finally:
-        if connection:
-            connection.close()
-            logger.info('Connection to database closed...')
-
-def make_connection(worker_id):
+def make_connection(worker_id=None) -> None:
     """
     Return a connection to the database, or return None.
 
@@ -45,7 +30,7 @@ def make_connection(worker_id):
     https://www.psycopg.org/docs/module.html#exceptions
     """
 
-    connection = None
+    global connection
 
     try:
         logger.info('Connecting to database...')
@@ -59,16 +44,16 @@ def make_connection(worker_id):
     except Exception as e:
         logger.exception(f'Unexpected error: {e}')
 
-    return connection
-
-def execute_remove_function(connection) -> None:
+def execute_remove_function(worker_id=None, is_retry: bool = False) -> None:
     """
     Executes the REMOVE_OPTED_OUT_RECORDS_QUERY statement and commits.
 
     Returns None 
     """
+    global connection
+
     if not connection:
-        logger.info('Failed to execute removal function...')
+        logger.info('Failed to execute removal function, no connection...')
         return
 
     try:
@@ -83,6 +68,24 @@ def execute_remove_function(connection) -> None:
     except psycopg2.Error as e:
         logger.exception(e)
         logger.error(e.pgcode)
+        logger.error(f'Possible lost connection to database, {"retrying" if not is_retry else "aborting"}...')
+        if not is_retry:
+            make_connection(worker_id)
+            execute_remove_function(worker_id, is_retry=True)
     except Exception as e:
         logger.error(f'Unexpected error')
         logger.exception(e)
+
+def va_profile_remove_old_opt_outs_handler(event=None, context=None, worker_id=None):
+    """
+    This function deletes any va_profile cache records that
+    are opted out and greater than 24 hours old.
+    """
+
+    # https://www.psycopg.org/docs/module.html#exceptions
+    try:
+        execute_remove_function(worker_id)
+    except Exception as e:
+        logger.exception(e)
+
+make_connection()
