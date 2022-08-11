cd1e4dab04134c9d5326f04adaa88c1d08233aca
Jesse Anderson <jesse.c.anderson@icloud.com>
2022-08-10 20:06:42 -0400

    trying again

--- tests/app/test_model.py
+++ tests/app/test_model.py
@@ -233,9 +233,7 @@ def test_letter_notification_serializes_with_subject(client, sample_letter_templ
 
 
 def test_user_service_role_serializes(client):
-    res = UserServiceRoles()
-    res.role = "admin"
-    assert res['role'] == "admin"
+    res = UserServiceRoles.serialize()
     assert res[id] is not None
 
 
