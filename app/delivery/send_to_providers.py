4a7cbe125b442cd044f60c075a4e7adb43c533b9
Evan Parish <evan.parish@oddball.io>
2022-08-31 15:06:19 -0400

    Quick fix for failing unit test

--- app/delivery/send_to_providers.py
+++ app/delivery/send_to_providers.py
@@ -83,18 +83,18 @@ def send_sms_to_provider(notification):
         message_service_sid = None
         is_twilio = isinstance(provider, TwilioSMSClient)
 
-        if is_twilio:
-            # This is an instance of ServiceSmsSender or None.
-            service_sms_sender = dao_get_service_sms_sender_by_service_id_and_number(
-                notification.service_id,
-                notification.reply_to_text
-            )
-
-            assert isinstance(service_sms_sender.sms_sender_specifics, dict), \
-                type(service_sms_sender.sms_sender_specifics)
-            message_service_sid = service_sms_sender.sms_sender_specifics.get("message_service_sid")
-
         try:
+            if is_twilio:
+                # This is an instance of ServiceSmsSender or None.
+                service_sms_sender = dao_get_service_sms_sender_by_service_id_and_number(
+                    notification.service_id,
+                    notification.reply_to_text
+                )
+
+                assert isinstance(service_sms_sender.sms_sender_specifics, dict), \
+                    type(service_sms_sender.sms_sender_specifics)
+                message_service_sid = service_sms_sender.sms_sender_specifics.get("message_service_sid")
+
             if message_service_sid is None:
                 # Send a SMS message using the "to" attribute to specify the recipient.
                 reference = provider.send_sms(
