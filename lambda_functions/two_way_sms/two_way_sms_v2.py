b3d0fa9567cf3d7b0bf12662c034886da02f4945
Kyle MacMillan <kyle.macmillan@oddball.io>
2022-11-09 21:23:35 -0500

    Added SQS retry and dead letter support. Changed format for globals. Added comments.

--- lambda_functions/two_way_sms/two_way_sms_v2.py
+++ lambda_functions/two_way_sms/two_way_sms_v2.py
@@ -1,6 +1,7 @@
 import boto3
 from botocore.client import BaseClient
 from datetime import datetime
+import json
 import logging
 from os import sys, environ
 import psycopg2
@@ -8,7 +9,7 @@ import requests
 
 
 # Global Variables
-# Immutable
+# Constants
 START_TYPES = ('START', 'BEGIN', 'RESTART', 'OPTIN', 'OPT-IN',)
 STOP_TYPES = ('STOP', 'OPTOUT', 'OPT-OUT',)
 HELP_TYPES = ('HELP',)
@@ -25,36 +26,44 @@ EXPECTED_SNS_FIELDS = set({'originationNumber',
                            'previousPublishedMessageId',
                            })
 
-# Pre-Configurable
+# Pre-defined env variables
 AWS_PINPOINT_APP_ID = ''
 AWS_REGION = ''
+DEAD_LETTER_SQS_URL = ''
 LOG_LEVEL = 'INFO'
 SQLALCHEMY_DATABASE_URI = ''
+RETRY_SQS_URL = ''
 TIMEOUT = 3
 
 # Set within lambda
 logger = None
-AWS_PINPOINT = None
-TWO_WAY_SMS_TABLE_DICT = {}
+aws_pinpoint_client = None
+aws_sqs_client = None
+two_way_sms_table_dict = {}
 
 
 def set_env_variables() -> None:
     """
     Attempt to get environmental variables. Abort execution environment on failure.
     """
-    global AWS_PINPOINT_APP_ID, AWS_REGION, LOG_LEVEL, SQLALCHEMY_DATABASE_URI, TIMEOUT
+    global AWS_PINPOINT_APP_ID,AWS_REGION,DEAD_LETTER_SQS_URL
+    global LOG_LEVEL,RETRY_SQS_URL,SQLALCHEMY_DATABASE_URI,TIMEOUT
 
     try:
         AWS_PINPOINT_APP_ID = environ['aws_pinpoint_app_id']
         AWS_REGION = environ['aws_region']
+        DEAD_LETTER_SQS_URL = environ['dead_letter_sqs_url']
         LOG_LEVEL = environ['log_level']
+        RETRY_SQS_URL = environ['retry_sqs_url']
         SQLALCHEMY_DATABASE_URI = environ['sql_alchemy_database_uri']
         TIMEOUT = environ['timeout']
     except KeyError as e:
         sys.exit(f'Failed to find env variable: {e}')
 
-
 def set_logger() -> None:
+    """
+    Sets custom logger for the lambda.
+    """
     global logger
     try:
         logger = logging.getLogger('TwoWaySMSv2')
@@ -87,11 +96,11 @@ def make_database_connection(worker_id: int = None) -> psycopg2.connection:
 
 def set_service_two_way_sms_table() -> None:
     """
-    Sets the TWO_WAY_SMS_TABLE_DICT if it is not set by opening a connection to the DB and 
+    Sets the two_way_sms_table_dict if it is not set by opening a connection to the DB and 
     querying the table. This table should be small (a few dozen records at most).
     """
-    # format for dict should be set to {'number':{'service_id': <value>, 'url_endpoint': <value>, 'self_managed': <value> }}
-    global TWO_WAY_SMS_TABLE_DICT
+    # format for dict should be: {'number':{'service_id': <value>, 'url_endpoint': <value>, 'self_managed': <value> }}
+    global two_way_sms_table_dict
     try:
         db_connection = make_database_connection()
         data = {}
@@ -102,11 +111,11 @@ def set_service_two_way_sms_table() -> None:
             logger.debug(f'Data returned from query: {data}')
         db_connection.close()
 
-        TWO_WAY_SMS_TABLE_DICT = {n: {'service_id': s,
+        two_way_sms_table_dict = {n: {'service_id': s,
                                       'url_endpoint': u,
                                       'self_managed': True if sm == 't' else False} for n, s, u, sm in data}
-        logger.info('TWO_WAY_SMS_TABLE_DICT set...')
-        logger.debug(f'Two way table as a dictionary with numbers as keys: {TWO_WAY_SMS_TABLE_DICT}')
+        logger.info('two_way_sms_table_dict set...')
+        logger.debug(f'Two way table as a dictionary with numbers as keys: {two_way_sms_table_dict}')
     except Exception as e:
         logger.critical(f'Failed to query database: {e}')
         if db_connection:
@@ -115,9 +124,14 @@ def set_service_two_way_sms_table() -> None:
 
 
 def init_execution_environment() -> None:
+    """
+    Collects environmental variables, sets up the logger, populates the two_way_sms_table_dict,
+    and sets up the aws pinpoint and sqs clients.
+    """
     set_env_variables()
     set_logger()
     set_service_two_way_sms_table()
+    set_aws_clients()
     logger.info('Execution environment setup...')
 
 
@@ -128,26 +142,29 @@ init_execution_environment()
 
 
 def two_way_sms_v2_handler(event: dict, context, worker_id=None):
+    """
+    Handler for inbound messages from SNS.
+    """
     if not valid_event():
-        push_to_dead_letter(event)
+        logger.critical(f'Logging entire event: {event}')
+        # Deadletter
+        push_to_sqs(event, False)
         return 500, 'Unrecognized event'
 
-    if not set_aws_pinpoint():
-        push_to_sqs(event)
-        return 400, 'Failed to setup Pinpoint client'
-
     for event_data in event.get('Records'):
-        inbound_sms = event_data.get('Sns')
         try:
+            inbound_sms = event_data.get('Sns')
+            is_sns = 'dateReceived' not in inbound_sms
+
             # Update and log SNS or SQS inbound_sms
-            if 'dateReceived' not in event:
-                logger.info('Processing SNS event...')
+            if is_sns:
+                logger.info('Processing SNS inbound_sms...')
                 inbound_sms['dateReceived'] = datetime.utcnow()
             else:
-                logger.info('Processing SQS event...')
+                logger.info('Processing SQS inbound_sms...')
 
             # Unsafe lookup intentional to catch missing record
-            two_way_record = TWO_WAY_SMS_TABLE_DICT[inbound_sms.get('originationNumber')]
+            two_way_record = two_way_sms_table_dict[inbound_sms.get('originationNumber')]
 
             # If the number is not self-managed, look for key words
             if not two_way_record.get('self_managed'):
@@ -160,30 +177,33 @@ def two_way_sms_v2_handler(event: dict, context, worker_id=None):
 
             # Forward inbound_sms to associated service
             logger.info(f'Forwarding inbound SMS to service: {two_way_record.get("service_id")}')
-            send_to_service(inbound_sms)
+            forward_to_service(inbound_sms)
         except KeyError as e:
             logger.exception(e)
             logger.critical(f'Unable to find two_way_record for: {inbound_sms.get("originationNumber")}')
-            push_to_dead_letter(inbound_sms)
+            push_to_sqs(inbound_sms, True, is_sns)
         except Exception as e:
             logger.exception(e)
-            push_to_sqs(inbound_sms)
+            # Deadletter
+            push_to_sqs(inbound_sms, False, is_sns)
     return 200, 'Success'
-
 
 def valid_event(event_data: dict) -> bool:
     """
     Ensure the event has all the necessary fields
     """
-    if 'Records' in event_data:
-        for event in event_data.get('Records'):
-            inbound_sms = event.get('Sns')
-            if not inbound_sms or not EXPECTED_SNS_FIELDS.issubset(inbound_sms):
-                logger.critical(f'Failed to detect critical fields in event: {event}')
-                return False
-
-    return True
-
+    try:
+        if 'Records' in event_data:
+            for record in event_data.get('Records'):
+                inbound_sms = record.get('Sns')
+                if not inbound_sms or not EXPECTED_SNS_FIELDS.issubset(inbound_sms):
+                    # Log specific record causing issues
+                    logger.critical(f'Failed to detect critical fields in record: {record}')
+                    return False
+        return True
+    except Exception as e:
+        logger.critical(f'Failed to parse event_data')
+        return False
 
 def detected_keyword(message: str) -> str:
     """
@@ -206,12 +226,19 @@ def detected_keyword(message: str) -> str:
         return ''
 
 
-def set_aws_pinpoint():
-    global AWS_PINPOINT
-    if AWS_PINPOINT is not None:
+def set_aws_clients():
+    global aws_pinpoint_client, aws_sqs_client
+    if aws_pinpoint_client is not None and aws_sqs_client is not None:
         return True
     try:
-        AWS_PINPOINT = boto3.client('pinpoint', region_name=AWS_REGION)
+        logger.info('Setting aws_pinpoint_client...')
+        aws_pinpoint_client = boto3.client('pinpoint', region_name=AWS_REGION)
+        logger.info('aws_pinpoint_client set...')
+        
+        logger.info('Setting aws_sqs_client...')
+        aws_sqs_client = boto3.client('sqs', region_name=AWS_REGION)
+        logger.info('aws_sqs_client set...')
+
         return True
     except Exception as e:
         logger.critical(f'Unable to set pinpoint client: {e}')
@@ -219,9 +246,13 @@ def set_aws_pinpoint():
 
 
 def send_message(recipient_number: str, sender: str, message: str) -> dict:
+    """
+    Called when we are monitoring for keywords and one was detected. This sends the 
+    appropriate response to the phone number that requested a message via keyword.
+    """
     try:
         # Should probably be smsv2
-        response = AWS_PINPOINT.send_messages(
+        response = aws_pinpoint_client.send_messages(
             ApplicationId=AWS_PINPOINT_APP_ID,
             MessageRequest={
                 'Addresses': {
@@ -245,27 +276,46 @@ def send_message(recipient_number: str, sender: str, message: str) -> dict:
         logger.critical(f'Failed to send message: {message} to {recipient_number} from {sender}')
 
 
-def send_to_service(inbound_sms: dict, url: str) -> None:
+def forward_to_service(inbound_sms: dict, url: str) -> None:
+    """
+    Forwards inbound SMS to the service that has 2-way SMS setup.
+    """
     try:
         logger.debug(f'Connecting to {url}, sending: {inbound_sms}')
         response = requests.post(url, data=inbound_sms, timeout=TIMEOUT)
-        # If we cannot get the json we want to raise, to push it to SQS
+        # If we cannot get the json, raise and push it to SQS
         logger.info(f'Response successful: {response.json()}')
     except Exception as e:
         logger.exception(e)
         logger.error(f'Failed to connect to {url}')
         raise
 
-
-def push_to_sqs(inbound_sms: dict) -> None:
+def push_to_sqs(inbound_sms: dict, is_retry: bool, is_sns: bool = 'unknown') -> None:
+    """
+    Pushes an inbound sms or entire event to SQS. Sends to RETRY or DEAD LETTER queue dependent
+    on is_retry variable. Also identifies the source (sns, sqs, or unknown).
+    """
     try:
-        logger.warn(f'Pushing event to SQS')
+        logger.warning(f'Pushing event to {"RETRY" if is_retry else "DEAD LETTER"} queue')
         logger.debug(f'Event: {inbound_sms}')
-        # push to SQS
+        
+        queue_msg = json.dumps(inbound_sms)
+        queue_msg_attrs = {
+            'source': {
+                'DataType': 'String',
+                'StringValue': is_sns
+            }
+        }
+
+        aws_sqs_client.send_message(QueueUrl=RETRY_SQS_URL if is_retry else DEAD_LETTER_SQS_URL,
+                                    MessageAttributes=queue_msg_attrs,
+                                    MessageBody=queue_msg)
+
+        logger.info('Completed enqueue of message')
     except Exception as e:
+        logger.exception(e)
         logger.critical(f'Failed to push event to SQS: {inbound_sms}')
-        push_to_dead_letter(inbound_sms)
-
-
-def push_to_dead_letter(inbound_sms: dict) -> None:
-    pass
+        if is_retry:
+            push_to_sqs(inbound_sms, False, is_sns)
+        else:
+            logger.critical(f'Attempt to enqueue to DEAD LETTER failed')
