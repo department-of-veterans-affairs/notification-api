dfcdec15cba462aaffba420a60b51468db68d438
trevor2718 <trevor@docme360.com>
2022-11-15 10:43:39 -0500

    pytest failure hooks

--- lambda_functions/user_flows/conftest.py
+++ lambda_functions/user_flows/conftest.py
@@ -1,3 +1,6 @@
+import pytest
+
+
 def pytest_addoption(parser):
     parser.addoption("--environment", action="store", default="some env")
 
@@ -10,25 +13,25 @@ def pytest_generate_tests(metafunc):
         metafunc.parametrize("environment", [option_value],  scope="session")
 
 
-# def pytest_runtest_logreport(report):
-#     if report.longrepr is None:
-#         return
-#     for tb_repr, *_ in report.longrepr.chain:
-#         for entry in tb_repr.reprentries:
-#             if entry.reprfuncargs is not None:
-#                 args = entry.reprfuncargs.args
-#                 for idx, (name, value) in enumerate(args):
-#                     if "service_test_api_key" in name:
-#                         args[idx] = (name, "********")
-#             if entry.reprlocals is not None:
-#                 lines = entry.reprlocals.lines
-#                 for idx, line in enumerate(lines):
-#                     if line.startswith("service_test_api_key"):
-#                         lines[idx] = "service_test_api_key          = '*********'"
+def pytest_runtest_logreport(report):
+    if report.longrepr is None:
+        return
+    for tb_repr, *_ in report.longrepr.chain:
+        for entry in tb_repr.reprentries:
+            if entry.reprfuncargs is not None:
+                args = entry.reprfuncargs.args
+                for idx, (name, value) in enumerate(args):
+                    if "service_test_api_key" in name:
+                        args[idx] = (name, "********")
+            if entry.reprlocals is not None:
+                lines = entry.reprlocals.lines
+                for idx, line in enumerate(lines):
+                    if line.startswith("service_test_api_key"):
+                        lines[idx] = "service_test_api_key          = '*********'"
 
 
-# @pytest.hookimpl(hookwrapper=True)
-# def pytest_runtest_makereport(item, call):
-#     out = yield
-#     report = out.get_result()
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    out = yield
+    report = out.get_result()
 
