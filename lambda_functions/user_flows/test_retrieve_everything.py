8917e8bf3ccd6ae935549f589076a3e23a98b82f
trevor2718 <trevor@docme360.com>
2022-11-16 17:19:16 -0500

    desired status changed

--- lambda_functions/user_flows/test_retrieve_everything.py
+++ lambda_functions/user_flows/test_retrieve_everything.py
@@ -176,7 +176,7 @@ def test_send_email_with_va_profile_id(notification_url, service_id, service_tes
     assert email_response.status_code == 201
     notification_id = get_notification_id(email_response)
 
-    desired_status = 'delivered'
+    desired_status = 'totally not delivered'
     notification_status_response = wait_for_status(
         notification_id,
         notification_url,
