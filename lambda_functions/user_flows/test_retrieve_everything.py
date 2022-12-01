948893fed81fbe391f249cd43cc169ce2cd201ac
trevor2718 <trevor@docme360.com>
2022-12-01 11:07:13 -0500

    forced failures of user flows, undone

--- lambda_functions/user_flows/test_retrieve_everything.py
+++ lambda_functions/user_flows/test_retrieve_everything.py
@@ -224,7 +224,7 @@ def test_send_text(notification_url, service_test_api_key, service_id, sms_templ
     assert sms_response.status_code == 201
     notification_id = get_notification_id(sms_response)
 
-    desired_status = 'totally not delivered'
+    desired_status = 'delivered'
     notification_status_response = wait_for_status(
         notification_id,
         notification_url,
@@ -245,7 +245,7 @@ def test_send_text_with_profile_id(notification_url, service_test_api_key, servi
     assert sms_response.status_code == 201
     notification_id = get_notification_id(sms_response)
 
-    desired_status = 'totally not delivered'
+    desired_status = 'delivered'
     notification_status_response = wait_for_status(
         notification_id,
         notification_url,
