b114ec275908f30f34ea1dde0c2958902c792e22
trevor2718 <trevor@docme360.com>
2022-12-02 13:15:42 -0500

    limited sensitive words

--- lambda_functions/user_flows/conftest.py
+++ lambda_functions/user_flows/conftest.py
@@ -11,7 +11,7 @@ def pytest_generate_tests(metafunc):
         metafunc.parametrize("environment", [option_value],  scope="session")
 
 # running list of substrings of secrets to mask in function below
-sensitive_words = ["bearer", "key", "token", "secret"]
+sensitive_words = ["key", "token"]
 
 def pytest_runtest_logreport(report):
     if report.longrepr is None:
