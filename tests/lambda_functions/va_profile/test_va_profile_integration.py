7d3dd059653c86f5fea884dd5cde743586fc3c2d
trevor2718 <trevor@docme360.com>
2022-12-02 10:11:17 -0500

    comment out force failure

--- tests/lambda_functions/va_profile/test_va_profile_integration.py
+++ tests/lambda_functions/va_profile/test_va_profile_integration.py
@@ -272,7 +272,8 @@ def test_jwt_is_valid(jwt_encoded, public_key):
     """
 
     assert jwt_is_valid(f"Bearer {jwt_encoded}", public_key)
-    assert jwt_encoded is None
+ # temp removal
+ #   assert jwt_encoded is None
 
 @pytest.mark.parametrize("invalid_jwt", [
     jwt_encoded_missing_exp, jwt_encoded_missing_iat, jwt_encoded_expired, jwt_encoded_reversed
