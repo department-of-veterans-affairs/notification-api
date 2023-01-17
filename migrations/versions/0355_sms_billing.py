330dea57cabd1ac349d72d30e9d219ea7d41e2d4
David J. Kalbfleisch <1.21e9W@protonmail.com>
2023-01-17 14:20:19 -0500

    squash - migration trial and error

--- migrations/versions/0355_sms_billing.py
+++ migrations/versions/0355_sms_billing.py
@@ -6,7 +6,6 @@ Create Date: 2023-01-11 20:09:58.840087
 
 from alembic import op
 import sqlalchemy as sa
-from sqlalchemy.dialects import postgresql
 
 revision = '0355_sms_billing'
 down_revision = '0354_notification_sms_sender_id'
@@ -15,13 +14,13 @@ down_revision = '0354_notification_sms_sender_id'
 def upgrade():
     op.add_column('notifications', sa.Column('segments_count', sa.Integer(), nullable=False))
     op.add_column('notifications', sa.Column('cost_in_millicents', sa.Float(), nullable=False))
-    op.add_column('notification_history', sa.Column('segments_count', sa.Integer(), nullable=False))
-    op.add_column('notification_history', sa.Column('cost_in_millicents', sa.Float(), nullable=False))
+    # op.add_column('notification_history', sa.Column('segments_count', sa.Integer(), nullable=False))
+    # op.add_column('notification_history', sa.Column('cost_in_millicents', sa.Float(), nullable=False))
 
 
 def downgrade():
-    op.drop_column('notification_history', 'cost_in_millicents')
-    op.drop_column('notification_history', 'segments_count')
+    # op.drop_column('notification_history', 'cost_in_millicents')
+    # op.drop_column('notification_history', 'segments_count')
     op.drop_column('notifications', 'cost_in_millicents')
     op.drop_column('notifications', 'segments_count')
 
