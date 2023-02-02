37588aafd17ca0804ad9a83067be4ca41607dd54
David J. Kalbfleisch <1.21e9W@protonmail.com>
2023-02-01 19:02:46 -0500

    Unit test fixes. #724

--- tests/app/dao/test_users_dao.py
+++ tests/app/dao/test_users_dao.py
@@ -376,13 +376,12 @@ def test_get_user_by_identity_provider_user_id(notify_db_session):
     assert user == user_from_db
 
 
-@pytest.mark.parametrize('initial_id_provider, expected_id_provider',
-                         [
-                             (None, "test-id"),
-                             ("old-id", "old-id")
-                         ])
+@pytest.mark.parametrize('initial_id_provider, expected_id_provider', [
+    (None, "test-id"),
+    ("old-id", "old-id"),
+])
 def test_update_user_identity_provider_user_id_for_identity_provider_when_none(
-        db_session, initial_id_provider, expected_id_provider
+        notify_db_session, initial_id_provider, expected_id_provider
 ):
     user = create_user(identity_provider_user_id=initial_id_provider)
 
