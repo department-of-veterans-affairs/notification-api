53df9efb9bc9800fca8fb3e2023edfa8d91e22e9
ldraney <draneylucas@gmail.com>
2023-03-10 17:06:34 -0700

    added profiler start to application.py; also added dynamic tagging for version and env

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from __future__ import print_function
 from ddtrace import patch_all
+from ddtrace.profiling import Profiler
 import os
 
 import sentry_sdk
@@ -28,3 +29,10 @@ create_app(application)
 
 # this starts the ddtrace tracer and configures it to the right port and URL
 patch_all()
+#This starts the Datadog profiler
+prof = Profiler(
+    env=os.environ.get("DD_ENV"),  # use the DD_ENV environment variable, or None if it's not set
+    service=os.environ.get("DD_SERVICE"),  # use the DD_SERVICE environment variable, or None if it's not set
+    version=os.environ.get("DD_VERSION"),  # use the DD_VERSION environment variable, or None if it's not set
+)
+prof.start()  # Should be as early as possible, eg before other imports, to ensure everything is profiled
