191389a2e51d2f19ab51a5d9d32fed8dfb5123d4
David Baldwin <124309521+justaskdavidb-docme360@users.noreply.github.com>
2023-03-21 10:58:27 -0500

    Update test_process_delivery_status_result_tasks.py
    * cleaned up code for test case

--- tests/app/celery/test_process_delivery_status_result_tasks.py
+++ tests/app/celery/test_process_delivery_status_result_tasks.py
@@ -1,4 +1,3 @@
-import base64
 import datetime
 import json
 
@@ -8,6 +7,7 @@ from app.feature_flags import FeatureFlag
 from tests.app.db import create_notification
 
 
+# db session variable is required parameter even though it is not used
 def test_passes_if_toggle_disabled(mocker, db_session):
     # set is_feature_enabled = False
     mock_toggle = mocker.patch('app.celery.process_delivery_status_result_tasks.is_feature_enabled', return_value=False)
@@ -35,9 +35,7 @@ def test_passes_if_toggle_disabled(mocker, db_session):
 def test_create_notification(mocker, db_session, sample_template):
     # make sure process delivery status results enabled
     mocker.patch('app.celery.process_delivery_status_result_tasks.is_feature_enabled', return_value=True)
-    mock_dao_get_notification_by_reference = mocker.patch(
-        'app.celery.process_delivery_status_result_tasks.dao_get_notification_by_reference'
-    )
+
     # create a notification
     test_reference = 'sms-reference-1'
     create_notification(sample_template, reference=test_reference, sent_at=datetime.datetime.utcnow(), status='sending')
@@ -54,66 +52,29 @@ def test_event_message(mocker, db_session, sample_template):
     # create a notification
     test_reference = 'sms-reference-1'
     create_notification(sample_template, reference=test_reference, sent_at=datetime.datetime.utcnow(), status='sending')
-    notification = notifications_dao.dao_get_notification_by_reference(test_reference)
-
     process_delivery_status_result_tasks.process_delivery_status(
         event=pdsr_notification_callback_record(
-            reference=test_reference,
-            event_type='_SMS.SUCCESS',
+            reference='reference-1',
+            provider='twilio',
             record_status='DELIVERED'
         )
     )
 
-# def test_callback_task(mocker, db_session, sample_template, event_type, record_status):
-#
-#     # make sure process delivery status results enabled
-#     mock_toggle = mocker.patch('app.celery.process_delivery_status_result_tasks.is_feature_enabled', return_value=True)
-#     mock_callback = mocker.patch('app.celery.process_delivery_status_result_tasks.check_and_queue_callback_task')
-#
-#     # create a notification
-#     test_reference = 'sms-reference-1'
-#     create_notification(sample_template, reference=test_reference, sent_at=datetime.datetime.utcnow(), status='sending')
-#     process_delivery_status_result_tasks.process_delivery_status(
-#         event=pdsr_notification_callback_record(
-#             reference=test_reference, event_type=event_type, record_status=record_status
-#         )
-#     )
-#
-#     notification = notifications_dao.dao_get_notification_by_reference(test_reference)
-#     assert notification.status == expected_notification_status
-#     mock_callback.assert_called_once()
-
 
 def pdsr_notification_callback_record(
         reference,
-        event_type='_SMS.SUCCESS',
-        record_status='DELIVERED'
+        provider='twilio',
+        record_status='delivered',
 ):
     process_delivery_status_result_task_message = {
-        "provider": "twilio",
-        "account_sid": "TWILIO_TEST_ACCOUNT_SID_XXX",
-        "api_version": "2010-04-01",
-        "body": "Hello! 👍",
-        "date_created": "Thu, 30 Jul 2015 20:12:31 +0000",
-        "date_sent": "Thu, 30 Jul 2015 20:12:33 +0000",
-        "date_updated": "Thu, 30 Jul 2015 20:12:33 +0000",
-        "direction": "outbound-api",
-        "error_code": None,
-        "error_message": None,
-        "from": "+18194120710",
-        "messaging_service_sid": "MGXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-        "num_media": "0",
-        "num_segments": "1",
-        "price": -0.00750,
-        "price_unit": "USD",
-        "sid": "MMXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-        "status": "sent",
-        "subresource_uris": {
-            "media": "/2010-04-01/Accounts/TWILIO_TEST_ACCOUNT_SID_XXX/Messages"
-                     "/SMXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Media.json"
-        },
-        "to": "+14155552345",
-        "uri": "/2010-04-01/Accounts/TWILIO_TEST_ACCOUNT_SID_XXX/Messages/SMXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX.json",
+        "provider": provider,
+        "body": {
+            "payload": "Hello! 👍",
+            "reference": reference,
+            "record_status": record_status,
+            "number_of_message_parts": 0,
+            "price_in_millicents_usd": 0
+        }
     }
 
     return {
