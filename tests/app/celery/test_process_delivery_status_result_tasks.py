d1bea11d1d1da8bba5cb750aa6c58a4412e77afa
David Baldwin <124309521+justaskdavidb-docme360@users.noreply.github.com>
2023-03-22 22:20:51 -0500

    1095 Twilio Delivery Status - ECS Entry Point #1121
    * added test case for when message key is incorrect or not present

--- tests/app/celery/test_process_delivery_status_result_tasks.py
+++ tests/app/celery/test_process_delivery_status_result_tasks.py
@@ -1,4 +1,5 @@
 import datetime
+import pytest
 from app.celery import process_delivery_status_result_tasks
 from app.dao import notifications_dao
 from app.clients.sms.twilio import TwilioSMSClient
@@ -44,3 +45,29 @@ def test_with_correct_data(mocker, db_session, sample_template, sample_service, 
 
     assert process_delivery_status_result_tasks.process_delivery_status(event=message)
 
+# we want to test that celery task will fail when invalid provider is given
+def test_without_message(mocker, db_session, sample_template, sample_service, sample_notification):
+    test_reference = 'sms-reference-1'
+    translate_return_value = {
+        "payload": "eyJhcmdzIjogW3siTWVzc2FnZSI6IHsiYm9keSI6ICJSYXdEbHJEb25lRGF0ZT0yMzAzMDkyMDI",
+        "reference": "MessageSID",
+        "record_status": "sent",
+    }
+    message = {'messag': {
+        'body': 'UmF3RGxyRG9uZURhdGU9MjMwMzIyMjMzOCZTbXNTaWQ9U014eHgmU21zU3RhdHV'
+                'zPWRlbGl2ZXJlZCZNZXNzYWdlU3RhdHVzPWRlbGl2ZXJlZCZUbz0lMkIxMTExMTExMTExMSZ'
+                'NZXNzYWdlU2lkPVNNeXl5JkFjY291bnRTaWQ9QUN6enomRnJvbT0lMkIxMjIyMzMzNDQ0NCZB'
+                'cGlWZXJzaW9uPTIwMTAtMDQtMDE=',
+        'provider': 'twilio'}}
+
+    mock_clients = mocker.patch('app.clients')
+    mock_sms_client = mocker.patch('app.clients.sms.SmsClient')
+    mock_twilio_client_method = mocker.patch('app.clients.sms.twilio.TwilioSMSClient.translate_delivery_status',
+                                             return_value=translate_return_value)
+    mock = mocker.patch(
+        'app.celery.process_delivery_status_result_tasks.attempt_to_get_notification',
+        return_value=(sample_notification, False, False)
+    )
+
+    with pytest.raises(Retry):
+        process_delivery_status_result_tasks.process_delivery_status(event=message)
