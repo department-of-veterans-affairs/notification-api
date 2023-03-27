f45d5b79c951b56f1fca1cdd803974aa4a681a93
Jacob Cheriathundam <jacobcheriathundam@gmail.com>
2023-03-27 13:24:18 -0400

    removed ddtrace from application.py because its unused

--- application.py
+++ application.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 from __future__ import print_function
-from ddtrace import patch_all
 import os
 
 import sentry_sdk
@@ -16,12 +15,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 sentry_sdk.init(
-    dsn=os.environ.get('SENTRY_URL', ''),
+    dsn=os.environ.get("SENTRY_URL", ""),
     integrations=[FlaskIntegration()],
-    release=os.environ.get('GIT_SHA', '')
+    release=os.environ.get("GIT_SHA", ""),
 )
 
-application = Flask('app')
+application = Flask("app")
 application.wsgi_app = ProxyFix(application.wsgi_app)
 create_app(application)
 
