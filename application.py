5463c3aee5088d35a49f5b7a1c6788a685089848
Jacob Cheriathundam <jacobcheriathundam@gmail.com>
2023-03-27 14:24:16 -0400

    re-introduced ddtrace push_all

--- application.py
+++ application.py
@@ -3,7 +3,7 @@ from __future__ import print_function
 import os
 
 import sentry_sdk
-
+from ddtrace import patch_all
 from flask import Flask
 from sentry_sdk.integrations.flask import FlaskIntegration
 from werkzeug.middleware.proxy_fix import ProxyFix
@@ -15,15 +15,15 @@ from dotenv import load_dotenv
 load_dotenv()
 
 sentry_sdk.init(
-    dsn=os.environ.get("SENTRY_URL", ""),
+    dsn=os.environ.get('SENTRY_URL', ''),
     integrations=[FlaskIntegration()],
-    release=os.environ.get("GIT_SHA", ""),
+    release=os.environ.get('GIT_SHA', '')
 )
 
-application = Flask("app")
+application = Flask('app')
 application.wsgi_app = ProxyFix(application.wsgi_app)
 create_app(application)
 
 
 # this starts the ddtrace tracer and configures it to the right port and URL
-# patch_all()
+patch_all()
