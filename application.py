a44e7e81a6812648932b427c2b4b4ec2adbc131b
Jacob Cheriathundam <jacobcheriathundam@gmail.com>
2023-03-27 16:54:43 -0400

    added in ddtrace

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 from __future__ import print_function
-# from ddtrace import patch_all
+from ddtrace import patch_all
 import os
 
 import sentry_sdk
@@ -16,15 +16,15 @@ from dotenv import load_dotenv
 load_dotenv()
 
 sentry_sdk.init(
-    dsn=os.environ.get('SENTRY_URL', ''),
+    dsn=os.environ.get("SENTRY_URL", ""),
     integrations=[FlaskIntegration()],
-    release=os.environ.get('GIT_SHA', '')
+    release=os.environ.get("GIT_SHA", ""),
 )
 
-application = Flask('app')
+application = Flask("app")
 application.wsgi_app = ProxyFix(application.wsgi_app)
 create_app(application)
 
 
 # this starts the ddtrace tracer and configures it to the right port and URL
-# patch_all()
+patch_all()
