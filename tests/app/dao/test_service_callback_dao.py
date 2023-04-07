0a96bf6fb174fc54c6611a126083bc5f2e3122e8
David Baldwin <124309521+justaskdavidb-docme360@users.noreply.github.com>
2023-04-07 12:14:45 -0500

    1197 - Add missing unit test for twilio (#1194)
    * refactor test_dao_get_callback_include_payload_status

--- tests/app/dao/test_service_callback_dao.py
+++ tests/app/dao/test_service_callback_dao.py
@@ -1,10 +1,12 @@
+import pytest
 from app.dao.service_callback_api_dao import (save_service_callback_api)
 from app.dao.service_callback_dao import (dao_get_callback_include_payload_status)
 from app.models import ServiceCallback, WEBHOOK_CHANNEL_TYPE, \
     NOTIFICATION_SENT, DELIVERY_STATUS_CALLBACK_TYPE
 
 
-def test_dao_service_callback(sample_service):
+@pytest.mark.parametrize('include_provider_payload', [True, False])
+def test_dao_get_callback_include_payload_status(sample_service, include_provider_payload):
     service_callback_api = ServiceCallback(
         service_id=sample_service.id,
         url="https://some_service/callback_endpoint",
@@ -13,11 +15,16 @@ def test_dao_service_callback(sample_service):
         callback_type=DELIVERY_STATUS_CALLBACK_TYPE,
         notification_statuses=NOTIFICATION_SENT,
         callback_channel=WEBHOOK_CHANNEL_TYPE,
-        include_provider_payload=True
+        include_provider_payload=include_provider_payload
     )
 
     save_service_callback_api(service_callback_api)
     include_payload_status = dao_get_callback_include_payload_status(
         service_id=service_callback_api.service_id,
         service_callback_type=DELIVERY_STATUS_CALLBACK_TYPE)
+
     assert isinstance(include_payload_status, bool)
+    assert include_payload_status is include_provider_payload
+
+
+
