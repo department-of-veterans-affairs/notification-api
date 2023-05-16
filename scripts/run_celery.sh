78d3c6512f63b678c2d2f0c171bebebcdf60ff2a
Kyle MacMillan <macattackftw@gmail.com>
2023-05-15 21:10:25 -0400

    Changed concurrency back

--- scripts/run_celery.sh
+++ scripts/run_celery.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-exec celery -A run_celery.notify_celery worker --pidfile="/tmp/celery.pid" --loglevel=INFO --concurrency=10
+exec celery -A run_celery.notify_celery worker --pidfile="/tmp/celery.pid" --loglevel=INFO --concurrency=4
