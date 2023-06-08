4f1bfd804fa0569a791d0cc36d751001593dd1ab
David J. Kalbfleisch <1.21e9W@protonmail.com>
2023-06-08 13:07:36 -0400

    squash - correct migration

--- migrations/versions/0359_communication_items_unique.py
+++ migrations/versions/0359_communication_items_unique.py
@@ -12,12 +12,6 @@ down_revision = "0358_default_send_field"
 
 
 def upgrade():
-    op.alter_column(
-        "communication_items", "name",
-        existing_type=sa.VARCHAR(),
-        type_=sa.Text(),
-        existing_nullable=False
-    )
     op.create_unique_constraint("communication_items_unique_name", "communication_items", ["name"])
     op.create_unique_constraint(
         "communication_items_unique_va_profile_item_id",
@@ -27,14 +21,5 @@ def upgrade():
 
 
 def downgrade():
-    # op.drop_constraint("communication_items_unique_va_profile_item_id", "communication_items", type_="unique")
-    # op.drop_constraint("communication_items_unique_name", "communication_items", type_="unique")
-    # TODO - These names are the ones automatically created.  Delete them after the downgrade.
-    op.drop_constraint("communication_items_va_profile_item_id_key", "communication_items", type_="unique")
-    op.drop_constraint("communication_items_name_key", "communication_items", type_="unique")
-    op.alter_column(
-        "communication_items", "name",
-        existing_type=sa.Text(),
-        type_=sa.VARCHAR(),
-        existing_nullable=False
-    )
+    op.drop_constraint("communication_items_unique_va_profile_item_id", "communication_items", type_="unique")
+    op.drop_constraint("communication_items_unique_name", "communication_items", type_="unique")
