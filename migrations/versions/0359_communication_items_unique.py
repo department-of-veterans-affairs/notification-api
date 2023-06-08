db452c7aab88dbc0b45a7fa3e3fffa391c2d8b8e
David J. Kalbfleisch <1.21e9W@protonmail.com>
2023-06-08 12:23:20 -0400

    squash - fixing downgrade issue

--- migrations/versions/0359_communication_items_unique.py
+++ migrations/versions/0359_communication_items_unique.py
@@ -18,13 +18,20 @@ def upgrade():
         type_=sa.Text(),
         existing_nullable=False
     )
-    op.create_unique_constraint(None, "communication_items", ["name"])
-    op.create_unique_constraint(None, "communication_items", ["va_profile_item_id"])
+    op.create_unique_constraint("communication_items_unique_name", "communication_items", ["name"])
+    op.create_unique_constraint(
+        "communication_items_unique_va_profile_item_id",
+        "communication_items",
+        ["va_profile_item_id"]
+    )
 
 
 def downgrade():
-    op.drop_constraint(None, "communication_items", type_="unique")
-    op.drop_constraint(None, "communication_items", type_="unique")
+    # op.drop_constraint("communication_items_unique_va_profile_item_id", "communication_items", type_="unique")
+    # op.drop_constraint("communication_items_unique_name", "communication_items", type_="unique")
+    # TODO - These names are the ones automatically created.  Delete them after the downgrade.
+    op.drop_constraint("communication_items_va_profile_item_id_key", "communication_items", type_="unique")
+    op.drop_constraint("communication_items_name_key", "communication_items", type_="unique")
     op.alter_column(
         "communication_items", "name",
         existing_type=sa.Text(),
