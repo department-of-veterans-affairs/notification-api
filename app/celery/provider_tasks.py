54eac12534f9bf1e782b9b956792a762eca7fc84
Kyle MacMillan <kyle.macmillan@oddball.io>
2023-07-21 11:31:35 -0400

    Changed retries to 2886 per policy

--- app/celery/provider_tasks.py
+++ app/celery/provider_tasks.py
@@ -123,7 +123,7 @@ def deliver_sms_with_rate_limiting(self, notification_id, sms_sender_id=None):
 @notify_celery.task(bind=True, name="deliver_email",
                     throws=(AutoRetryException, ),
                     autoretry_for=(AutoRetryException, ),
-                    max_retries=6, retry_backoff=True, retry_backoff_max=60)
+                    max_retries=2886, retry_backoff=True, retry_backoff_max=60)
 @statsd(namespace="tasks")
 def deliver_email(self, notification_id: str, sms_sender_id=None):
     try:
