a7d400750bb03bd7ea540a9b84affab510caeed0
Kyle MacMillan <kyle.macmillan@oddball.io>
2023-07-21 15:26:29 -0400

    Added type annotations

--- app/celery/common.py
+++ app/celery/common.py
@@ -1,3 +1,5 @@
+import logging
+
 from app.dao.notifications_dao import update_notification_status_by_id
 from app.models import NOTIFICATION_TECHNICAL_FAILURE
 
@@ -5,12 +7,12 @@ from app.models import NOTIFICATION_TECHNICAL_FAILURE
 RETRIES_EXCEEDED = "Retries exceeded"
 
 
-def can_retry(retries: int, max_retries: int):
+def can_retry(retries: int, max_retries: int) -> bool:
     """ Facilitates testing - Compares retries vs max retries returns True if retries < max_retries """
     return retries < max_retries
 
 
-def handle_max_retries_exceeded(notification_id: str, method_name: str, logger) -> str:
+def handle_max_retries_exceeded(notification_id: str, method_name: str, logger: logging.Logger) -> str:
     """ Handles sms/email deliver requests that exceeded the retry maximum, updates Notification status """
     logger.critical("%s: Notification %s failed by exceeding retry limits", method_name, notification_id)
     message = ("RETRY FAILED: Max retries reached. "
