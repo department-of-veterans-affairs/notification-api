3d2108880ce61e9c6db8a63294024f149ded50a9
Nikolai Efimov <efimov.nikolai@gmail.com>
2023-08-04 17:11:39 -0700

    fixed "secret" violates not-null constraint" error

--- app/dao/api_key_dao.py
+++ app/dao/api_key_dao.py
@@ -15,9 +15,9 @@ from sqlalchemy import or_, func
 @transactional
 @version_class(ApiKey)
 def save_model_api_key(api_key):
-    if api_key.id is not None:
+    if not api_key.id:
         api_key.id = uuid.uuid4()  # must be set now so version history model can use same id
-    if api_key.secret is not None:
+    if not api_key.secret:
         api_key.secret = uuid.uuid4()
     db.session.add(api_key)
 
