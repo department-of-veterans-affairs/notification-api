0a8e79976466d005132d4349ac57be5d608a9c6a
Nikolai Efimov <efimov.nikolai@gmail.com>
2023-08-23 08:11:32 -0700

    remove useless test

--- tests/app/letter_branding/test_letter_branding_rest.py
+++
@@ -1,8 +0,0 @@
-import uuid
-
-from tests import create_authorization_header
-
-
-def test_get_letter_branding_by_id_returns_404_if_does_not_exist(client, notify_db_session):
-    response = client.get('/letter-branding/{}'.format(uuid.uuid4()), headers=[create_authorization_header()])
-    assert response.status_code == 404
