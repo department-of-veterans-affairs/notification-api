be4b5c697ce2111d602939da8024d4caa72f47c2
Kyle MacMillan <kyle.w.macmillan@gmail.com>
2023-11-09 10:24:46 -0500

    Marked test for skip

--- tests/app/api_key/test_rest.py
+++ tests/app/api_key/test_rest.py
@@ -1,6 +1,7 @@
 from app import DATETIME_FORMAT
 from app.models import Service, EMAIL_TYPE
 from datetime import datetime
+import pytest
 from tests.app.db import create_notification
 
 
@@ -52,6 +53,7 @@ def test_get_api_key_stats_no_sends(notify_db_session, admin_request, sample_api
     assert api_key_stats["last_send"] is None
 
 
+@pytest.mark.skip(reason="Endpoint disabled and slated for removal")
 def test_get_api_keys_ranked(
     notify_db_session,
     admin_request,
