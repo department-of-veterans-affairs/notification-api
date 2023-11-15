1eb5f43b5a0a5a647dc073ed7955a0accf982f22
EvanParish <evan.parish@oddball.io>
2023-11-15 11:29:53 -0600

    fix unit test

--- app/config.py
+++ app/config.py
@@ -342,8 +342,7 @@ class Config(object):
             'send-scheduled-comp-and-pen-sms': {
                 'task': 'send-scheduled-comp-and-pen-sms',
                 # At every minute past every hour from 13 through 21 on every day-of-month from 21 through 28
-                # TODO test: use this as real value after testing - crontab(hour='13-21', day_of_month='21-28'),
-                'schedule': crontab(minute='0,15,30,45'),
+                'schedule': crontab(hour='13-21', day_of_month='21-28'),
                 'options': {'queue': QueueNames.PERIODIC}
             },
         },
