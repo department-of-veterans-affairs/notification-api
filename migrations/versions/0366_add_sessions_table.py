46456025c19ca8b5417242c6f9f74d8591dc62d0
Michael Marchand <35391349+MarchandMD@users.noreply.github.com>
2023-11-30 16:12:58 -0700

    adds migration for sessions

---
+++ migrations/versions/0366_add_sessions_table.py
@@ -0,0 +1,31 @@
+"""
+
+Revision ID: 6392698ad84a
+Revises: 0365_add_notification_failures
+Create Date: 2023-11-30 21:40:23.546531
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
+
+revision = '0366_add_sessions_table'
+down_revision = '0365_add_notification_failures'
+
+
+def upgrade():
+    op.create_table('sessions',
+    sa.Column('id', postgresql.UUID(as_uuid=True), nullable=False),
+    sa.Column('session_id', postgresql.UUID(as_uuid=True), nullable=False),
+    sa.Column('data', postgresql.JSONB(astext_type=sa.Text()), nullable=True),
+    sa.Column('created_at', sa.DateTime(), nullable=False),
+    sa.Column('updated_at', sa.DateTime(), nullable=True),
+    )
+
+    op.create_index('unique_session_id', 'sessions', ['session_id'], unique=True)
+    op.create_index('index_session_updated_at', 'sessions', ['updated_at'])
+
+def downgrade():
+    op.drop_index('index_session_updated_at', table_name='sessions')
+    op.drop_index('unique_session_id', table_name='sessions')
+    op.drop_table('sessions')
