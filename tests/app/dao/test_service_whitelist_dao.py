7e29f434d34983bfc73eb9099f3ed36724a27b15
Kyle MacMillan <kyle.w.macmillan@gmail.com>
2023-12-04 12:47:25 -0500

    Refactored for new fixtures

--- tests/app/dao/test_service_whitelist_dao.py
+++ tests/app/dao/test_service_whitelist_dao.py
@@ -1,41 +1,63 @@
 import uuid
 
+from sqlalchemy import delete, select
+
 from app.models import (
     ServiceWhitelist,
     EMAIL_TYPE,
 )
-
 from app.dao.service_whitelist_dao import (
     dao_fetch_service_whitelist,
     dao_add_and_commit_whitelisted_contacts,
     dao_remove_service_whitelist
 )
-from tests.app.db import create_service
 
 
-def test_fetch_service_whitelist_gets_whitelists(sample_service_whitelist):
-    whitelist = dao_fetch_service_whitelist(sample_service_whitelist.service_id)
+def test_fetch_service_whitelist_gets_whitelists(
+    sample_service_whitelist,
+):
+
+    service_whitelist = sample_service_whitelist()
+    whitelist = dao_fetch_service_whitelist(service_whitelist.service_id)
     assert len(whitelist) == 1
-    assert whitelist[0].id == sample_service_whitelist.id
+    assert whitelist[0].id == service_whitelist.id
 
 
-def test_fetch_service_whitelist_ignores_other_service(sample_service_whitelist):
+def test_fetch_service_whitelist_ignores_other_service(
+    sample_service_whitelist,
+):
+
+    sample_service_whitelist()
     assert len(dao_fetch_service_whitelist(uuid.uuid4())) == 0
 
 
-def test_add_and_commit_whitelisted_contacts_saves_data(notify_db_session, sample_service):
-    whitelist = ServiceWhitelist.from_string(sample_service.id, EMAIL_TYPE, 'foo@example.com')
+def test_add_and_commit_whitelisted_contacts_saves_data(
+    notify_db_session,
+    sample_service,
+):
+
+    service = sample_service()
+    whitelist = ServiceWhitelist.from_string(service.id, EMAIL_TYPE, 'foo@example.com')
 
     dao_add_and_commit_whitelisted_contacts([whitelist])
 
-    db_contents = ServiceWhitelist.query.all()
+    stmt = select(ServiceWhitelist).where(ServiceWhitelist.service_id == service.id)
+    db_contents = notify_db_session.session.scalars(stmt).all()
     assert len(db_contents) == 1
     assert db_contents[0].id == whitelist.id
 
+    # Teardown
+    notify_db_session.session.delete(whitelist)
+    notify_db_session.session.commit()
 
-def test_remove_service_whitelist_only_removes_for_my_service(notify_db_session):
-    service_1 = create_service(service_name="service 1")
-    service_2 = create_service(service_name="service 2")
+
+def test_remove_service_whitelist_only_removes_for_my_service(
+    notify_db_session,
+    sample_service,
+):
+
+    service_1 = sample_service()
+    service_2 = sample_service()
     dao_add_and_commit_whitelisted_contacts([
         ServiceWhitelist.from_string(service_1.id, EMAIL_TYPE, 'service1@example.com'),
         ServiceWhitelist.from_string(service_2.id, EMAIL_TYPE, 'service2@example.com')
@@ -46,11 +68,24 @@ def test_remove_service_whitelist_only_removes_for_my_service(notify_db_session)
     assert service_1.whitelist == []
     assert len(service_2.whitelist) == 1
 
+    # Teardown
+    # service_1 is already marked, just needs to be commit
+    notify_db_session.session.execute(delete(ServiceWhitelist).where(ServiceWhitelist.service_id == service_2.id))
+    notify_db_session.session.commit()
 
-def test_remove_service_whitelist_does_not_commit(notify_db_session, sample_service_whitelist):
-    dao_remove_service_whitelist(sample_service_whitelist.service_id)
+
+def test_remove_service_whitelist_does_not_commit(
+    notify_db_session,
+    sample_service_whitelist,
+):
+
+    service_whitelist = sample_service_whitelist()
+    dao_remove_service_whitelist(service_whitelist.service_id)
 
     # since dao_remove_service_whitelist doesn't commit, we can still rollback its changes
     notify_db_session.session.rollback()
 
-    assert ServiceWhitelist.query.count() == 1
+    assert notify_db_session.session.get(ServiceWhitelist, service_whitelist.id)
+
+    notify_db_session.session.delete(service_whitelist)
+    notify_db_session.session.commit()
