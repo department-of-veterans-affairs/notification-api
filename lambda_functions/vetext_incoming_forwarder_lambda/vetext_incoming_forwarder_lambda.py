dc712c857c9f9c84b6c5f5fe3c7ce79cc0844b29
edSynapse <ed.cass@synapsefi.com>
2023-12-11 08:56:58 -0800

    #1189 Debugging logging for stage

--- lambda_functions/vetext_incoming_forwarder_lambda/vetext_incoming_forwarder_lambda.py
+++ lambda_functions/vetext_incoming_forwarder_lambda/vetext_incoming_forwarder_lambda.py
@@ -42,6 +42,7 @@ def validate_twilio_event(event):
     decoded = base64.b64decode(event.get("body")).decode()
     params = parse_qs(decoded)
     params = {k: v[0] for k, v in params.items()}
+    logger.error(params)
     return validator.validate(
         uri=uri,
         params=params,
