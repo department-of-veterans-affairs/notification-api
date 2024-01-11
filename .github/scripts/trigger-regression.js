dfe031ce0eebffc1834f2971b3125b1f186f3f08
ldraney <draneylucas@gmail.com>
2024-01-11 16:27:01 -0700

    trying another workflow

--- .github/scripts/trigger-regression.js
+++ .github/scripts/trigger-regression.js
@@ -12,9 +12,9 @@ const { ENVIRONMENT } = process.env;
 const triggerAndWait = async ({ github, context, core }) => {
   const owner = 'department-of-veterans-affairs'; // user of private repo 
   const repo = 'notification-api-qa'; // private repo to contact
-  const workflow_id = 'regression.yml'; // Replace with your workflow file name or ID
-  const ref = '1526-test-branch'; // Usually main or master.  THIS IS THE REF of the REGRESSION repo!
-  const jobName = 'Test in ${ENVIRONMENT}'; // Replace with the name of the job you want
+  const workflow_id = 'build.yml'; // Replace with your workflow file name or ID
+  const ref = 'master'; // Usually main or master.  THIS IS THE REF of the REGRESSION repo!
+  // const jobName = 'Test in ${ENVIRONMENT}'; // Replace with the name of the job you want
 
   // Create a timestamp for workflow run tracking
   const triggerTimestamp = new Date().toISOString();
@@ -71,38 +71,38 @@ const triggerAndWait = async ({ github, context, core }) => {
   console.log(`Workflow run URL: ${workflow_url}`);
 
   // Fetch the job within the workflow run
-  const jobs = await github.rest.actions.listJobsForWorkflowRun({
-    owner,
-    repo,
-    run_id,
-  });
+  // const jobs = await github.rest.actions.listJobsForWorkflowRun({
+    // owner,
+    // repo,
+    // run_id,
+  // });
 
-  const job = jobs.data.jobs.find(j => j.name === jobName);
-  if (!job) {
-    console.log(`Job '${jobName}' not found in workflow run.`);
-    return;
-  }
+  // const job = jobs.data.jobs.find(j => j.name === jobName);
+  // if (!job) {
+    // console.log(`Job '${jobName}' not found in workflow run.`);
+    // return;
+  // }
 
-  let job_id = job.id;
+  // let job_id = job.id;
 
-  // Fetch and handle the job logs
-  github.rest.actions.downloadJobLogsForWorkflowRun({
-    owner,
-    repo,
-    job_id,
-  }).then(response => {
-    console.log(`Job logs: ${response.data}`);
-  }).catch(error => {
-    console.log('Error fetching job logs:', error);
-  });
+  // // Fetch and handle the job logs
+  // github.rest.actions.downloadJobLogsForWorkflowRun({
+    // owner,
+    // repo,
+    // job_id,
+  // }).then(response => {
+    // console.log(`Job logs: ${response.data}`);
+  // }).catch(error => {
+    // console.log('Error fetching job logs:', error);
+  // });
 
-   // Set the output for the job summary
-  const resultText = conclusion === 'success' ? 'passed' : 'failed';
-  core.setOutput('regression_result', `QA Regression result is ${resultText}; link to this run is ${workflow_url}`);
+   // // Set the output for the job summary
+  // const resultText = conclusion === 'success' ? 'passed' : 'failed';
+  // core.setOutput('regression_result', `QA Regression result is ${resultText}; link to this run is ${workflow_url}`);
 
-  // Append to GITHUB_STEP_SUMMARY
-  const summaryContent = `### Workflow Result\nResult: ${resultText}\n[Link to Workflow Run](${workflow_url})`;
-  require('fs').appendFileSync(process.env.GITHUB_STEP_SUMMARY, summaryContent);
+  // // Append to GITHUB_STEP_SUMMARY
+  // const summaryContent = `### Workflow Result\nResult: ${resultText}\n[Link to Workflow Run](${workflow_url})`;
+  // require('fs').appendFileSync(process.env.GITHUB_STEP_SUMMARY, summaryContent);
 
   // Check if the workflow failed and set an appropriate error message
   if (conclusion !== 'success') {
