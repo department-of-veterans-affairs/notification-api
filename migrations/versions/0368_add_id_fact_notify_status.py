2a0b59a0506ca5550ec496dc797b3cf5d37ad191
Michael Wellman <michaelwellman@MichaelacStudio.lan>
2024-02-15 08:09:26 -0500

    Added the uuid-ossp extension

--- migrations/versions/0368_add_id_fact_notify_status.py
+++ migrations/versions/0368_add_id_fact_notify_status.py
@@ -14,6 +14,9 @@ down_revision = '0367_add_auth_parameter'
 
 
 def upgrade():
+    # Create the ossp-uuid extension if it doesn't exist
+    op.execute('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";')
+    
     op.add_column('ft_notification_status', sa.Column('id', postgresql.UUID(as_uuid=True)))
     op.execute('ALTER TABLE ft_notification_status DROP CONSTRAINT ft_notification_status_pkey;')
     op.execute('UPDATE ft_notification_status SET id = uuid_generate_v4();')
