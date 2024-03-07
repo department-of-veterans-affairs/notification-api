f4236cd4bef3d033e391d96e524fbbe4acb3b0b9
Kyle MacMillan <kyle.w.macmillan@gmail.com>
2024-03-07 12:59:43 -0500

    Reverted

--- scripts/save_certificate.sh
+++ scripts/save_certificate.sh
@@ -6,9 +6,6 @@ then
   echo "bypassing VAProfile cert and key file generation"
 else
   echo "Writing SSL certificate and key to files"
-  ls -l /app/certs/
-  echo "this is a test" > /app/certs/kwm.txt
-  ls -l /app/certs
   echo "$VANOTIFY_SSL_CERT" > $VANOTIFY_SSL_CERT_PATH
   echo "$VANOTIFY_SSL_KEY" > $VANOTIFY_SSL_KEY_PATH
 fi
