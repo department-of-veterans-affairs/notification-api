aa259534f217b3566635014771b9e203f86aadd4
Michael Wellman <michaelwellman@MichaelacStudio.lan>
2024-03-28 08:08:17 -0400

    Removed personal names from comment

--- lambda_functions/vetext_incoming_forwarder_lambda/twilio_signature_utils.py
+++ lambda_functions/vetext_incoming_forwarder_lambda/twilio_signature_utils.py
@@ -95,7 +95,7 @@ def generate_twilio_signature_and_body(
 
 if __name__ == '__main__':
     # How to generate a test body and signature
-    # To test real events you need VEText's token. Ask for help from Cris or Kyle. Tokens are not shared with the team.
+    # To test real events you need VEText's Twilio token, this one is fake.
     token = '12345678'
     rv = RequestValidator(token)
 
