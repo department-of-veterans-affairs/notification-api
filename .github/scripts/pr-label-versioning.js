9489e0888eee784ba22c93c8e206016d24fb43ba
ldraney <draneylucas@gmail.com>
2024-04-22 19:47:28 -0600

    just pr payload

--- .github/scripts/pr-label-versioning.js
+++ .github/scripts/pr-label-versioning.js
@@ -3,15 +3,18 @@ function prData({ github, context, core }) {
     // Extract the pull request object from the GitHub context
     const pullRequest = context.payload.pull_request;
 
+	core.info(pullRequest)
+
+
     // Check if there are labels and print them
-    if (pullRequest && pullRequest.labels && pullRequest.labels.length > 0) {
-        core.info('Pull Request Labels:');
-        pullRequest.labels.forEach(label => {
-            core.info(`- ${label.name} (Color: ${label.color}, Description: ${label.description || 'No description provided.'})`);
-        });
-    } else {
-        core.info('No labels found on this pull request.');
-    }
+    // if (pullRequest && pullRequest.labels && pullRequest.labels.length > 0) {
+        // core.info('Pull Request Labels:');
+        // pullRequest.labels.forEach(label => {
+            // core.info(`- ${label.name} (Color: ${label.color}, Description: ${label.description || 'No description provided.'})`);
+        // });
+    // } else {
+        // core.info('No labels found on this pull request.');
+    // }
 }
 
 module.exports = prData;
