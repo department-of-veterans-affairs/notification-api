d2ded903fe3ed08429bc07e7717a9273fe5ff166
ldraney <draneylucas@gmail.com>
2024-04-22 20:04:56 -0600

    test

--- .github/scripts/pr-label-versioning.js
+++ .github/scripts/pr-label-versioning.js
@@ -1,20 +1,21 @@
 // File: .github/scripts/pr-label-versioning.js
-function prData({ github, context, core }) {
+// function prData({ github, context, core }) {
     // Extract the pull request object from the GitHub context
-    const pullRequest = context.payload.pull_request;
 
-	core.info(pullRequest)
+const prData = async ({ context }) => {
+  const pullRequestData = context.payload.pull_request
+  console.log(context)
+  console.log(pullRequestData)
 
-
-    // Check if there are labels and print them
-    // if (pullRequest && pullRequest.labels && pullRequest.labels.length > 0) {
-        // core.info('Pull Request Labels:');
-        // pullRequest.labels.forEach(label => {
-            // core.info(`- ${label.name} (Color: ${label.color}, Description: ${label.description || 'No description provided.'})`);
-        // });
-    // } else {
-        // core.info('No labels found on this pull request.');
-    // }
+	// Check if there are labels and print them
+	// if (pullRequest && pullRequest.labels && pullRequest.labels.length > 0) {
+		// core.info('Pull Request Labels:');
+		// pullRequest.labels.forEach(label => {
+			// core.info(`- ${label.name} (Color: ${label.color}, Description: ${label.description || 'No description provided.'})`);
+		// });
+	// } else {
+		// core.info('No labels found on this pull request.');
+	// }
 }
 
 module.exports = prData;
