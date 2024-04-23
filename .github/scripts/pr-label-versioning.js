b1c849ef5747d314dad3a94d02e4221a6761f7e3
ldraney <draneylucas@gmail.com>
2024-04-23 14:44:49 -0600

    run through POC

--- .github/scripts/pr-label-versioning.js
+++ .github/scripts/pr-label-versioning.js
@@ -14,17 +14,15 @@ const prData = async ({ github, context, core }) => {
   }
 
   const pullRequestData = context.payload.pull_request;
-  const labels = pullRequestData.labels; // Extract labels from the pull request data
-
-  const label = labels[0].name.toLowerCase();
+  const labels = pullRequestData.labels.map(label => label.name.toLowerCase());
   let versionParts = currentVersion.split('.').map(x => parseInt(x));
 
-  // Define version bump logic
-  if (label === 'breaking change') {
+  // Define version bump logic based on the first label
+  if (labels.includes('breaking change')) {
     versionParts[0] += 1; // Major version bump
     versionParts[1] = 0; // Reset minor version
     versionParts[2] = 0; // Reset patch version
-  } else if (['hotfix', 'security', 'bug'].includes(label)) {
+  } else if (labels.some(label => ['hotfix', 'security', 'bug'].includes(label))) {
     versionParts[2] += 1; // Patch version bump
   } else {
     versionParts[1] += 1; // Minor version bump
@@ -32,20 +30,19 @@ const prData = async ({ github, context, core }) => {
   }
 
   const newVersion = versionParts.join('.');
-  console.log("The new version will be: ", newVersion);
+  core.setOutput("new_version", newVersion);
 
-  core.setOutput("The new version will be: ", newVersion);
-  // core.setOutput("This is due to the semver value of: " + semverValue + " from the label " + label + ", which is being applied to the latest release tag: " + latestTagString);
+  const createTag = core.getInput('create-tag', { required: false }) === 'true';
+  const summaryMessage = createTag 
+    ? `A new tag ${newVersion} is created based on the ${labels.join(', ')} label(s).`
+    : `The new version will be ${newVersion} based on the ${labels.join(', ')} label(s).`;
 
-  // Append to GITHUB_STEP_SUMMARY
-  // May not be visibile until the pipeline actually finishes; in which case the actual wording here may need to be udpated.
   const summaryContent = `
-  The new version will be ${newVersion} based on the ${label} label.
-  Latest current release tag is ${currentVersion}.
+    ${summaryMessage}
+    Latest current release tag is ${currentVersion}.
   `;
   require('fs').appendFileSync(process.env.GITHUB_STEP_SUMMARY, summaryContent);
-
-}
+};
 
 module.exports = prData;
 
