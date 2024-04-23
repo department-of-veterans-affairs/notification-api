9396ae74a7acb2b69a463cad2affe1d95acdf93b
ldraney <draneylucas@gmail.com>
2024-04-23 10:05:00 -0600

    architecting logic

--- .github/scripts/pr-label-versioning.js
+++ .github/scripts/pr-label-versioning.js
@@ -1,28 +1,29 @@
 const prData = async ({ github, context, core }) => {
-  // const octokit = new Octokit({ auth: 'YOUR-TOKEN' });
-  
-  // Extract the owner and repo from the context
   const owner = context.repo.owner;
   const repo = context.repo.repo;
 
-  // Fetch the latest release to get the current version tag
   const latestRelease = await github.rest.repos.getLatestRelease({
     owner,
     repo,
   });
 
-  console.log("The tag name is ", latestRelease.data.tag_name)
+  // Get the tag string by parsing latestRelease.data.tag_name.  Output should be X.X.X, regardless if there was a vX.X.X or not with the latest release
+  // The output of that data.tag_name could be vX.X.X or X.X.X, but its important that there is no v in the new proposed version.
+
+  // Get the label on the PR (there should only be one; if there is more than one, error out with "there is more than one label".)
+  const pullRequestData = context.payload.pull_request;
+  const labels = pullRequestData.labels; // Extract labels from the pull request data
+
+
+  // Version bump that tag string for major.minor.patch, with the following logic:
+	// Major version bump label: breaking change
+	// Patch version bump: hotfix, security, bug fix
+	// Minor version bump: every other label
+	// Should two labels exist on a PR, only one should be used for versioning, with preference towards major, then minor, then patch.
+
+  // print the new proposed version bump like console.log("The new version will be: ", versionBump)
 
 }
 
-// const prData = async ({ context }) => {
-  // const pullRequestData = context.payload.pull_request;
-  // const labels = pullRequestData.labels; // Extract labels from the pull request data
-
-  // // Logging each label separately
-  // labels.forEach(label => {
-    // console.log(`Label Name: ${label.name}, Label Color: ${label.color}, Label Description: ${label.description}`);
-  // });
-// }
-
 module.exports = prData;
+
