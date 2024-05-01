f26514ed55af69ab7811f5ccc0d637869b6ba0e7
ldraney <draneylucas@gmail.com>
2024-05-01 13:57:46 -0600

    getting labels from new payload - stringify

--- .github/scripts/prData.js
+++ .github/scripts/prData.js
@@ -12,16 +12,16 @@ const prData = async ({ github, context, core }) => {
   try {
     console.log(`MergeSha is ${mergeSHA}`);
 
-    const { pullRequestData } = await github.rest.repos.listPullRequestsAssociatedWithCommit({
+    const pullRequestData = await github.rest.repos.listPullRequestsAssociatedWithCommit({
       owner,
       repo,
       commit_sha: mergeSHA,
     });
 
-	// console.log("the pullRequestData is: " + pullRequestData.data)
-	// console.log("the pullRequestData is: " + JSON.stringify(pullRequestData, null, 2));
-	console.log("the pull request number is " +  pullRequestData.data.number)
-	console.log("the pull request labels are " +  pullRequestData.data.labels)
+	console.log("the pullRequestData is: " + pullRequestData.data)
+	console.log("the pullRequestData is: " + JSON.stringify(pullRequestData, null, 2));
+	console.log("the pull request number is " +  JSON.stringify(pullRequestData.data.number));
+	console.log("the pull request labels are " +  JSON.stringify(pullRequestData.data.labels));
 
     // Assuming we can get the PR number from the first PR associated with the commit
     prNumber = pullRequestData.data.number;
