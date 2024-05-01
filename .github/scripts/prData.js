2e5b551d9c47bf3a1145e009fa93940feb3d9dea
ldraney <draneylucas@gmail.com>
2024-05-01 15:12:19 -0600

    readding release branch SHA

--- .github/scripts/prData.js
+++ .github/scripts/prData.js
@@ -41,14 +41,13 @@ function processLabelsAndVersion(labels, currentVersion) {
     };
 }
 
-
 // Function to fetch release branch SHA
 async function fetchReleaseBranchSha(github, owner, repo) {
     try {
         const { data } = await github.rest.repos.getCommit({
             owner,
             repo,
-            ref: "heads/release"
+            ref: "heads/release",
         });
         if (!data || !data.sha) {
             throw new Error("Release branch SHA not found.");
