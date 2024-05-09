047eb8a95749a58ebe498ccfcb5c7e86bb5226d5
David J. Kalbfleisch <1.21e9W@protonmail.com>
2024-05-09 15:04:46 -0400

    squash

--- app/celery/scheduled_tasks.py
+++ app/celery/scheduled_tasks.py
@@ -338,6 +338,8 @@ def send_scheduled_comp_and_pen_sms():
 
     try:
         # If this line doesn't raise ValueError, the value is a valid UUID.
+        # TODO - for testing; delete
+        current_app.logger.info('send_scheduled_comp_and_pen_sms sms_sender_id = %s', sms_sender_id)
         sms_sender_id = UUID(sms_sender_id)
         current_app.logger.info('Using the SMS sender ID specified in SSM Parameter store.')
     except ValueError:
