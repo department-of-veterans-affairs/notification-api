0757a62d3a77ebae5d74e6d663c586aca008c1aa
ldraney <draneylucas@gmail.com>
2024-05-14 11:08:07 -0600

    process.env works - PREVIOUS_VERSION

--- .github/scripts/createReleaseNotes.js
+++ .github/scripts/createReleaseNotes.js
@@ -2,16 +2,14 @@
 
 async function createReleaseNotes(params) {
   const { github, context, core } = params;
+  const { PREVIOUS_VERSION } = process.env;
 
   try {
     // Log the entire inputs object to the GitHub Actions summary
     core.summary.addRaw(`Inputs received: ${JSON.stringify(process.inputs)}`).write();
 
-    // Retrieve the previous version from the inputs
-    // const previousVersion = context.inputs.PREVIOUS_VERSION;
-
     // Output the previous version to the console
-    console.log(`The previous release version was: ${previousVersion}`);
+    console.log(`The previous release version was: ${PREVIOUS_VERSION}`);
   } catch (error) {
     core.setFailed(`Failed to generate summary: ${error.message}`);
     console.error(error);
