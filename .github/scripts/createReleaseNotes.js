44bec78ccd9d2c09943751e127fcb9c689004d6b
ldraney <draneylucas@gmail.com>
2024-05-14 10:58:16 -0600

    process.env vs context.inputs - object check

--- .github/scripts/createReleaseNotes.js
+++ .github/scripts/createReleaseNotes.js
@@ -1,21 +1,22 @@
 // createReleaseNotes.js
 
-
 async function createReleaseNotes(params) {
   const { github, context, core } = params;
-  // const { previousVersion } = context.inputs.previous_version
-  const previousVersion = context.inputs.previous_version
 
   try {
-	// Retrieve the previous version from the environment variables
-	// const previousVersion = process.env.PREVIOUS_VERSION;
+    // Log the entire inputs object to the GitHub Actions summary
+    core.summary.addRaw(`Inputs received: ${JSON.stringify(context.inputs)}`).write();
 
-	// Output the previous version to the console
-	console.log(`The previous release version was: ${previousVersion}`);
+    // Retrieve the previous version from the inputs
+    const previousVersion = context.inputs.PREVIOUS_VERSION;
+
+    // Output the previous version to the console
+    console.log(`The previous release version was: ${previousVersion}`);
   } catch (error) {
-	core.setFailed(`Failed to generate summary: ${error.message}`);
+    core.setFailed(`Failed to generate summary: ${error.message}`);
     console.error(error);
   }
-} 
+}
 
 module.exports = createReleaseNotes;
+
