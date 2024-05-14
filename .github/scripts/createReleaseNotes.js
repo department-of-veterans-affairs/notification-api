06db8f845951cb5621d8ca87e244e2e219332118
ldraney <draneylucas@gmail.com>
2024-05-14 10:25:40 -0600

    trying to get Previous version value - js

--- .github/scripts/createReleaseNotes.js
+++ .github/scripts/createReleaseNotes.js
@@ -1,9 +1,17 @@
 // createReleaseNotes.js
 
-// Retrieve the previous version from the environment variables
-const previousVersion = process.env.PREVIOUS_VERSION;
 
-// Output the previous version to the console
-console.log(`The previous release version was: ${previousVersion}`);
+async function createReleaseNotes() {
+  try {
+	// Retrieve the previous version from the environment variables
+	const previousVersion = process.env.PREVIOUS_VERSION;
+
+	// Output the previous version to the console
+	console.log(`The previous release version was: ${previousVersion}`);
+  } catch (error) {
+	core.setFailed(`Failed to generate summary: ${error.message}`);
+    console.error(error);
+  }
+} 
 
 module.exports = createReleaseNotes;
