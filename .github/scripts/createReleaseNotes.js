2b3bc9e9552670df94a18652b134145edba03e76
ldraney <draneylucas@gmail.com>
2024-05-14 10:44:12 -0600

    process.env vs context.inputs

--- .github/scripts/createReleaseNotes.js
+++ .github/scripts/createReleaseNotes.js
@@ -2,9 +2,12 @@
 
 
 async function createReleaseNotes() {
+  const { github, context, core } = params;
+  const { previousVersion } = context.inputs.PREVIOUS_VERSION
+
   try {
 	// Retrieve the previous version from the environment variables
-	const previousVersion = process.env.PREVIOUS_VERSION;
+	// const previousVersion = process.env.PREVIOUS_VERSION;
 
 	// Output the previous version to the console
 	console.log(`The previous release version was: ${previousVersion}`);
