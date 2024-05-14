797e7b1399f799baa3a98bfba433270a6127fdfc
ldraney <draneylucas@gmail.com>
2024-05-14 07:51:29 -0600

    create release notes basic setup

---
+++ .github/scripts/createReleaseNotes.js
@@ -0,0 +1,8 @@
+// createReleaseNotes.js
+
+// Retrieve the previous version from the environment variables
+const previousVersion = process.env.PREVIOUS_VERSION;
+
+// Output the previous version to the console
+console.log(`The previous release version was: ${previousVersion}`);
+
