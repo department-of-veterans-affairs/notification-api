2ce77811501955876073c28337470ac0523b6795
Mackenzie Halliday <mackenzie.halliday@gmail.com>
2024-07-08 14:32:56 -0500

    Squash 1836 - implementation spike

--- app/googleanalytics/ga4.py
+++ app/googleanalytics/ga4.py
@@ -1,32 +1,71 @@
 import os
 from urllib.parse import urlencode
+
 from flask import current_app
+import requests
+from requests.exceptions import RequestException
 
-def build_ga4_open_email_event_url(notification, provider):
+# code from first iternation
+# def build_ga4_open_email_event_url(notification, provider):
     
-    url_params_dict = {
-        'measurement_id': os.environ['GOOGLE_ANALYTICS_MEASUREMENT_ID'],
-        'api_secret': os.environ['GOOGLE_ANALYTICS_API_SECRET'],
+#     url_params_dict = {
+#         'measurement_id': os.environ['GOOGLE_ANALYTICS_MEASUREMENT_ID'],
+#         'api_secret': os.environ['GOOGLE_ANALYTICS_API_SECRET'],
 
-    }
+#     }
 
-    url_str = current_app.config['GOOGLE_ANALYTICS_GA4_URL']
-    # https://www.google-analytics.com/mp/
-    url_params = urlencode(url_params_dict)
-    url = f'{url_str}?{url_params}'
+#     url_str = current_app.config['GOOGLE_ANALYTICS_GA4_URL']
+#     # https://www.google-analytics.com/mp/
+#     url_params = urlencode(url_params_dict)
+#     url = f'{url_str}?{url_params}'
 
-    return url
+#     return url
 
-def build_ga4_open_email_event_body(notification, provider):
+# def build_ga4_open_email_event_body(notification, provider):
     
-    return {
-        "events": [{
-            "name": "open_email",
-            "params": {
-                "campaign_id": notification.template.name,
-                "campaign": notification.template.id,
-                "source": "vanotify",
-                "medium": "email",
-            }
-        }]
-    }
+#     return {
+#         "events": [{
+#             "name": "open_email",
+#             "params": {
+#                 "campaign_id": notification.template.name,
+#                 "campaign": notification.template.id,
+#                 "source": "vanotify",
+#                 "medium": "email",
+#             }
+#         }]
+#     }
+
+
+def send_ga4_open_email_event(notification, provider):
+    try:
+        # Build URL
+        url_params_dict = {
+            'measurement_id': os.environ['GOOGLE_ANALYTICS_MEASUREMENT_ID'],
+            'api_secret': os.environ['GOOGLE_ANALYTICS_API_SECRET'],
+        }
+        url_str = current_app.config['GOOGLE_ANALYTICS_GA4_URL']
+        url_params = urlencode(url_params_dict)
+        url = f'{url_str}?{url_params}'
+
+        content = f"template_id={notification.template.id}/service_id={notification.service.id}/service_name={notification.service.name}"
+
+        event_body = {
+            "events": [{
+                "name": "open_email",
+                "params": {
+                    "campaign_id": notification.template.id,
+                    "campaign": notification.template.name,
+                    "source": "vanotify",
+                    "medium": "email",
+                    "content": content,
+                }
+            }]
+        }
+
+        response = requests.post(url, json=event_body)
+
+        return response
+
+    except RequestException as error:
+        current_app.logger.error('Error sending GA4 event: %s', error)
+        return None
