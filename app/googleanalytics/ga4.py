90c4315856e26e5ce23a0c985b863c332a8b3e68
David J. Kalbfleisch <1.21e9W@protonmail.com>
2024-07-10 16:07:03 -0400

    1853 Create the route GET /ga4/open-email-tracking.

---
+++ app/googleanalytics/ga4.py
@@ -0,0 +1,45 @@
+"""
+Google Analytics 4
+"""
+
+from app.googleanalytics.ga4_schemas import ga4_request_schema
+from flask import current_app, Blueprint, request
+from jsonschema import FormatChecker, ValidationError
+from jsonschema.validators import Draft202012Validator
+
+ga4_blueprint = Blueprint('ga4', __name__, url_prefix='/ga4')
+
+ga4_request_validator = Draft202012Validator(
+    ga4_request_schema, format_checker=FormatChecker(['uuid'])
+)
+
+
+@ga4_blueprint.route('/open-email-tracking', methods=['GET'])
+def get_ga4():
+    """
+    This route is what alerts to the opening of an e-mail message.
+
+    https://flask.palletsprojects.com/en/3.0.x/api/#flask.Request.args
+    """
+
+    url_parameters_dict = request.args.to_dict()
+
+    # This could raise ValidationError.
+    ga4_request_validator.validate(url_parameters_dict)
+
+    current_app.logger.info(request.query_string)
+
+    # "No Content"
+    return {}, 204
+
+
+@ga4_blueprint.errorhandler(ValidationError)
+def ga4_schema_validation_error(error):
+    return {
+        'errors': [
+            {
+                'error': 'ValidationError',
+                'message': error.message,
+            }
+        ]
+    }, 400
