f2f3c27c65bbda212477d692354ede4cc9569dfb
Mackenzie Halliday <mackenzie.halliday@gmail.com>
2024-07-11 13:07:50 -0500

    Squash #1847 - Test needs to have context

--- tests/app/googleanalytics/test_pixels.py
+++ tests/app/googleanalytics/test_pixels.py
@@ -4,18 +4,25 @@ from app.googleanalytics.pixels import build_dynamic_ga4_pixel_tacking_url
 class TestGA4PixelTrackingURL:
     def test_build_dynamic_ga4_pixel_tacking_url_contains_expected_parameters(
         self,
+        notify_api,
         sample_notification_model_with_organization,
     ):
-        url = build_dynamic_ga4_pixel_tacking_url(sample_notification_model_with_organization)
+        notify_api.config['SERVER_NAME'] = 'localhost'
+        notify_api.config['APPLICATION_ROOT'] = '/'
+        notify_api.config['PREFERRED_URL_SCHEME'] = 'http'
+
+        with notify_api.app_context():
+
+            url = build_dynamic_ga4_pixel_tacking_url(sample_notification_model_with_organization)
 
 
-        all_expected_parameters = [
-            'campaign=',
-            'campaign_id=',
-            'name=email_opens',
-            'source=vanotify',
-            'medium=email',
-            'content=',
-        ]
+            all_expected_parameters = [
+                'campaign=',
+                'campaign_id=',
+                'name=email_opens',
+                'source=vanotify',
+                'medium=email',
+                'content=',
+            ]
 
-        assert all(parameter in url for parameter in all_expected_parameters)
+            assert all(parameter in url for parameter in all_expected_parameters)
