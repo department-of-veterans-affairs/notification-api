f394a7314200a998cb43cc17997ee50ccbc6ae64
Mackenzie Halliday <mackenzie.halliday@gmail.com>
2024-07-12 13:30:01 -0500

    Squash #1847 - Try to create dynamic url with root url determined by environment variable. Test failing - issues with for_url not finding a SERVER_NAME. SERVER_NAME seems to be set by the request. Not sure if it issue with the unit tests and I need to add context to the test OR if url_for cannot be used in this situation

--- app/googleanalytics/pixels.py
+++ app/googleanalytics/pixels.py
@@ -1,23 +1,48 @@
+import os
 from flask import current_app, url_for
+
+TEST_DOMAIN = "https://test-api.va.gov/notify/"
+DEV_DOMAIN = "https://dev-api.va.gov/notify/"
+PERF_DOMAIN = "https://sandbox-api.va.gov/notify/"
+STAGING_DOMAIN = "https://staging-api.va.gov/notify/"
+PROD_DOMAIN = "https://api.va.gov/notify/"
+
+
+def get_domain_for_environment():
+    #TODO move into helper file 
+    environment = os.getenv('NOTIFY_ENVIRONMENT', 'development')
+
+
+    ENVIRONMENT_DOMAINS = {
+        'test': TEST_DOMAIN,
+        'development': DEV_DOMAIN,
+        'perf': PERF_DOMAIN,
+        'staging': STAGING_DOMAIN,
+        'prod': PROD_DOMAIN,
+    }
+
+    return ENVIRONMENT_DOMAINS[environment]
 
 
 def build_dynamic_ga4_pixel_tracking_url(notification):
     """
     Constructs a dynamic URL that contains information on the notification email being sent.
     The dynamic URL is used to for pixel tracking and sends a request to our application when
-    email is openned.  
+    email is opened.
 
     :param notification: The notification object containing template and service details.
     :return: A dynamically constructed URL string.
     """
 
-    ga4_url = url_for('ga4.get_ga4', _external=True)
+    domain = get_domain_for_environment()
+    ga4_url = url_for('ga4.get_ga4', _external=False)
     url = (
+        f'{domain}'
         f'{ga4_url}?'
         f'campaign={notification.template.name}&campaign_id={notification.template.id}&'
         f'name=email_opens&source=vanotify&medium=email&'
         f'content={notification.service.name}/{notification.service.id}/{notification.id}'
     )
 
-    current_app.logger.info(f'Generated google analytics 4 pixel URL: {url}')
+    current_app.logger.info(f'Generated Google Analytics 4 pixel URL: {url}')
     return url
--- tests/app/googleanalytics/test_pixels.py
+++ tests/app/googleanalytics/test_pixels.py
@@ -1,22 +1,39 @@
+import os
+import pytest
+from unittest.mock import patch
 from app.googleanalytics.pixels import build_dynamic_ga4_pixel_tracking_url
 
+@pytest.mark.parametrize("env, expected_domain", [
+    ('test', 'https://test-api.va.gov/notify/'),
+    ('development', 'https://dev-api.va.gov/notify/'),
+    ('perf', 'https://sandbox-api.va.gov/notify/'),
+    ('staging', 'https://staging-api.va.gov/notify/'),
+    ('prod', 'https://api.va.gov/notify/'),
+])
+def test_build_dynamic_ga4_pixel_tracking_url_correct_domain_for_environment(
+    notify_api,
+    sample_notification_model_with_organization,
+    env,
+    expected_domain,
+):
+    with patch.dict(os.environ, {'NOTIFY_ENVIRONMENT': env}):
+        url = build_dynamic_ga4_pixel_tracking_url(sample_notification_model_with_organization)
+        assert expected_domain in url
 
-class TestGA4PixelTrackingURL:
-    def test_build_dynamic_ga4_pixel_tracking_url_contains_expected_parameters(
-        self,
-        notify_api,
-        sample_notification_model_with_organization,
-    ):
-        with notify_api.test_request_context():
-            url = build_dynamic_ga4_pixel_tracking_url(sample_notification_model_with_organization)
+def test_build_dynamic_ga4_pixel_tracking_url_contains_expected_parameters(
+    notify_api,
+    sample_notification_model_with_organization,
+):
+    with patch.dict(os.environ, {'NOTIFY_ENVIRONMENT': 'test'}):
+        url = build_dynamic_ga4_pixel_tracking_url(sample_notification_model_with_organization)
 
-            all_expected_parameters = [
-                'campaign=',
-                'campaign_id=',
-                'name=email_opens',
-                'source=vanotify',
-                'medium=email',
-                'content=',
-            ]
+        all_expected_parameters = [
+            'campaign=',
+            'campaign_id=',
+            'name=email_opens',
+            'source=vanotify',
+            'medium=email',
+            'content=',
+        ]
 
-            assert all(parameter in url for parameter in all_expected_parameters)
+        assert all(parameter in url for parameter in all_expected_parameters)
