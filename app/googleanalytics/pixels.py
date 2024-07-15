055ddd25255ed0f2fc24f8fc9b636795fa35708a
Mackenzie Halliday <mackenzie.halliday@gmail.com>
2024-07-15 11:10:50 -0400

    Squash #1847 - Update creating dynamic email based on test with QA

--- app/googleanalytics/pixels.py
+++ app/googleanalytics/pixels.py
@@ -1,16 +1,18 @@
 import os
-
 from flask import current_app
+from urllib.parse import quote
 
-
-TEST_DOMAIN = 'https://test-api.va.gov/notify/'
-DEV_DOMAIN = 'https://dev-api.va.gov/notify/'
-PERF_DOMAIN = 'https://sandbox-api.va.gov/notify/'
-STAGING_DOMAIN = 'https://staging-api.va.gov/notify/'
-PROD_DOMAIN = 'https://api.va.gov/notify/'
+TEST_DOMAIN = 'https://test-api.va.gov/vanotify/'
+DEV_DOMAIN = 'https://dev-api.va.gov/vanotify/'
+PERF_DOMAIN = 'https://sandbox-api.va.gov/vanotify/'
+STAGING_DOMAIN = 'https://staging-api.va.gov/vanotify/'
+PROD_DOMAIN = 'https://api.va.gov/vanotify/'
 
 NOTIFICATION_API_GA4_GET_ENDPOINT = 'ga4/open-email-tracking'
 
+GA4_PIXEL_TRACKING_NAME = 'email_open'
+GA4_PIXEL_TRACKING_SOURCE = 'vanotify'
+GA4_PIXEL_TRACKING_MEDIUM = 'email'
 
 def get_domain_for_environment():
     environment = os.getenv('NOTIFY_ENVIRONMENT', 'development')
@@ -25,11 +27,10 @@ def get_domain_for_environment():
 
     return ENVIRONMENT_DOMAINS[environment]
 
-
 def build_dynamic_ga4_pixel_tracking_url(notification):
     """
     Constructs a dynamic URL that contains information on the notification email being sent.
-    The dynamic URL is used to for pixel tracking and sends a request to our application when
+    The dynamic URL is used for pixel tracking and sends a request to our application when
     email is opened.
 
     :param notification: The notification object containing template and service details.
@@ -41,9 +42,9 @@ def build_dynamic_ga4_pixel_tracking_url(notification):
     url = (
         f'{domain}'
         f'{ga4_url}?'
-        f'campaign={notification.template.name}&campaign_id={notification.template.id}&'
-        f'name=email_opens&source=vanotify&medium=email&'
-        f'content={notification.service.name}/{notification.service.id}/{notification.id}'
+        f'campaign={quote(notification.template.name)}&campaign_id={quote(str(notification.template.id))}&'
+        f'name={quote(GA4_PIXEL_TRACKING_NAME)}&source={quote(GA4_PIXEL_TRACKING_SOURCE)}&medium={quote(GA4_PIXEL_TRACKING_MEDIUM)}&'
+        f'content={quote(notification.service.name)}/{quote(str(notification.service.id))}/{quote(str(notification.id))}'
     )
 
     current_app.logger.info(f'Generated Google Analytics 4 pixel URL: {url}')
--- tests/app/googleanalytics/test_pixels.py
+++ tests/app/googleanalytics/test_pixels.py
@@ -5,15 +5,14 @@ import pytest
 
 from app.googleanalytics.pixels import build_dynamic_ga4_pixel_tracking_url
 
-
 @pytest.mark.parametrize(
     'env, expected_domain',
     [
-        ('test', 'https://test-api.va.gov/notify/'),
-        ('development', 'https://dev-api.va.gov/notify/'),
-        ('perf', 'https://sandbox-api.va.gov/notify/'),
-        ('staging', 'https://staging-api.va.gov/notify/'),
-        ('prod', 'https://api.va.gov/notify/'),
+        ('test', 'https://test-api.va.gov/vanotify/'),
+        ('development', 'https://dev-api.va.gov/vanotify/'),
+        ('perf', 'https://sandbox-api.va.gov/vanotify/'),
+        ('staging', 'https://staging-api.va.gov/vanotify/'),
+        ('prod', 'https://api.va.gov/vanotify/'),
     ],
 )
 def test_build_dynamic_ga4_pixel_tracking_url_correct_domain_for_environment(
@@ -26,7 +25,6 @@ def test_build_dynamic_ga4_pixel_tracking_url_correct_domain_for_environment(
         url = build_dynamic_ga4_pixel_tracking_url(sample_notification_model_with_organization)
         assert expected_domain in url
 
-
 def test_build_dynamic_ga4_pixel_tracking_url_contains_expected_parameters(
     notify_api,
     sample_notification_model_with_organization,
@@ -37,10 +35,23 @@ def test_build_dynamic_ga4_pixel_tracking_url_contains_expected_parameters(
         all_expected_parameters = [
             'campaign=',
             'campaign_id=',
-            'name=email_opens',
+            'name=email_open',
             'source=vanotify',
             'medium=email',
             'content=',
         ]
 
         assert all(parameter in url for parameter in all_expected_parameters)
+
+def test_build_dynamic_ga4_pixel_tracking_url_encodes_spaces(
+    notify_api,
+    sample_notification_model_with_organization,
+):
+    with patch.dict(os.environ, {'NOTIFY_ENVIRONMENT': 'test'}):
+        sample_notification_model_with_organization.template.name = 'Test Campaign'
+        sample_notification_model_with_organization.service.name = 'Test Service'
+        
+        url = build_dynamic_ga4_pixel_tracking_url(sample_notification_model_with_organization)
+
+        assert 'Test%20Campaign' in url
+        assert 'Test%20Service' in url
