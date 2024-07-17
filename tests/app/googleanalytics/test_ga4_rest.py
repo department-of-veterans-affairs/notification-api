c65a5297d5ba434879db717aa0ff5b7626865d1b
Mackenzie Halliday <mackenzie.halliday@gmail.com>
2024-07-17 15:24:10 -0400

    Squash #1847 - Remove comment and add assert for image name

--- tests/app/googleanalytics/test_ga4_rest.py
+++ tests/app/googleanalytics/test_ga4_rest.py
@@ -3,7 +3,7 @@
 from flask import url_for
 
 
-def test_get_ga4_valid_data(client, ga4_request_data):
+def test_ut_get_ga4_valid_data(client, ga4_request_data):
     """
     A GET request with valid URL parameters should receive a 204 ("No Content") response.
     """
@@ -12,13 +12,13 @@ def test_get_ga4_valid_data(client, ga4_request_data):
         path=url_for('ga4.get_ga4'),
         query_string=ga4_request_data,
     )
-    # Errors - FileNotFoundError: [Errno 2] No such file or directory:
-    # /.venv/lib/python3.10/site-packages/test/images/pixel.png'
+
     assert response.status_code == 200, response.get_json()
     assert response.headers['Content-Type'].startswith('image/')
+    assert 'ga4_pixel_tracking.png' in response.headers['Content-Disposition']
 
 
-def test_get_ga4_invalid_data(client):
+def test_ut_get_ga4_invalid_data(client):
     """
     A GET request with invalid URL parameters should receive a 400 ("Bad Request") response.
     Test this by omitting all URL parameters.  Other tests validate the schema.
