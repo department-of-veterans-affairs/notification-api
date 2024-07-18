dd36c62b2d831362f2c46701ebdb69f4c602200b
Mackenzie Halliday <mackenzie.halliday@gmail.com>
2024-07-18 11:37:15 -0400

    Squash #1847 - Please work

--- app/googleanalytics/ga4.py
+++ app/googleanalytics/ga4.py
@@ -14,10 +14,8 @@ ga4_blueprint = Blueprint('ga4', __name__, url_prefix='/ga4')
 
 ga4_request_validator = Draft202012Validator(ga4_request_schema, format_checker=FormatChecker(['uuid']))
 
-if os.environ.get('GITHUB_WORKFLOW') == 1:
-    GA4_PIXEL_TRACKING_IMAGE_PATH = 'images/ga4_pixel_tracking.png'
-else:
-    GA4_PIXEL_TRACKING_IMAGE_PATH = '/app/images/ga4_pixel_tracking.png'
+GA4_PIXEL_TRACKING_IMAGE_PATH = f'{os.getcwd()}/images/ga4_pixel_tracking.png'
+print(GA4_PIXEL_TRACKING_IMAGE_PATH)
 
 
 @ga4_blueprint.route('/open-email-tracking', methods=['GET'])
