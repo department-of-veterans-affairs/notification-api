739aa8654c77dfcf64d11e83be4c2b6c27ca79d8
Michael Wellman <mchlwellman@gmail.com>
2024-07-18 15:47:36 -0400

    Added GA4 params to development

--- app/config.py
+++ app/config.py
@@ -508,6 +508,11 @@ class Development(Config):
 
     PUBLIC_DOMAIN = 'https://dev-api.va.gov/vanotify/'
 
+    GA4_URL = os.getenv('GA4_URL', 'https://www.google-analytics.com/mp/collect')
+    GA4_MEASUREMENT_ID = os.getenv('GA4_MEASUREMENT_ID', '')
+    logging.critical('GA4_MEASUREMENT_ID: %s', GA4_MEASUREMENT_ID)
+    GA4_API_SECRET = os.getenv('GA4_API_SECRET', '')
+
 
 class Test(Development):
     # When a service is created, this gets saved as default sms_sender
