2025f62bb647d5f9e91a0e1fe600ac89584aac7f
Mackenzie Halliday <mackenzie.halliday@gmail.com>
2024-10-08 16:55:25 -0400

    Squash #805 - Add template content

--- migrations/versions/0371_replace_template_content.py
+++ migrations/versions/0371_replace_template_content.py
@@ -1,27 +1,47 @@
 """
-Revision ID: 371_replace_template_history_content
+Revision ID: 371_replace_template_content
 Revises: 0370_notification_callback_url
 Create Date: 2024-10-08 20:35:38
 """
 from alembic import op
 from sqlalchemy import text
 
-revision = '371_replace_template_history_content'
+revision = '371_replace_template_content'
 down_revision = '0370_notification_callback_url'
 
 def upgrade():
+    op.execute(
+        text("""
+            UPDATE templates
+            SET name = REPLACE(name, :old_name, :new_name),
+                content = REPLACE(content, :old_url, :new_url)
+        """),
+        {
+         "old_url": "https://notification.alpha.canada.ca", 
+         "new_url": "https://api.va.gov/vanotify"}
+    )
+
     op.execute(
         text("""
             UPDATE templates_history
             SET name = REPLACE(name, :old_name, :new_name),
                 content = REPLACE(content, :old_url, :new_url)
         """),
-        {"old_name": "GOV.UK", "new_name": "", 
+        {
          "old_url": "https://notification.alpha.canada.ca", 
          "new_url": "https://api.va.gov/vanotify"}
     )
 
 def downgrade():
+    op.execute(
+        text("""
+            UPDATE templates
+            SET content = REPLACE(content, :new_url, :old_url)
+        """),
+        {"old_url": "https://notification.alpha.canada.ca", 
+         "new_url": "https://api.va.gov/vanotify"}
+    )
+  
     op.execute(
         text("""
             UPDATE templates_history
