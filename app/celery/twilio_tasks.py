735b75fb0918343052afb1f4fd81017b9345ba40
Michael Wellman <mchlwellman@gmail.com>
2024-10-31 07:54:19 -0400

    Ruff

--- app/celery/twilio_tasks.py
+++ app/celery/twilio_tasks.py
@@ -8,14 +8,19 @@ from app.models import Notification
 
 TWILIO_STATUS_PAGE_SIZE = 5
 
-def _get_notifications()->list:
+
+def _get_notifications() -> list:
     """
     Returns a list of notifications not in final state
     """
-    current_app.logger.info("Getting notifications to update status")
+    current_app.logger.info('Getting notifications to update status')
     # query = select([Notification]).where(Notification.status != 'delivered').limit(TWILIO_STATUS_PAGE_SIZE)
-    query = select([Notification]).where(Notification.status.in_(NOTIFICATION_STATUS_TYPES_COMPLETED)).limit(TWILIO_STATUS_PAGE_SIZE)
-    current_app.logger.debug("Query: %s", query)
+    query = (
+        select([Notification])
+        .where(Notification.status.in_(NOTIFICATION_STATUS_TYPES_COMPLETED))
+        .limit(TWILIO_STATUS_PAGE_SIZE)
+    )
+    current_app.logger.debug('Query: %s', query)
     return db.session.execute(query).all()
 
 
@@ -26,7 +31,7 @@ def update_twilio_status():
     """
     notifications = _get_notifications()
     for notification in notifications:
-        current_app.logger.info("Updating status for notification %s", notification.id)
+        current_app.logger.info('Updating status for notification %s', notification.id)
 
         # The twilio message sid is in the 'reference' field
         message_sid = notification.reference
