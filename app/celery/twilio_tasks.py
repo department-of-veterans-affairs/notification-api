fc662648664915d17ea34dc3055434224f8cf18f
Michael Wellman <mchlwellman@gmail.com>
2024-10-31 06:26:12 -0400

    WIP

--- app/celery/twilio_tasks.py
+++ app/celery/twilio_tasks.py
@@ -2,6 +2,7 @@ from flask import current_app
 from sqlalchemy import select
 
 from app import db, notify_celery, twilio_sms_client
+from app.constants import NOTIFICATION_STATUS_TYPES_COMPLETED
 from app.models import Notification
 
 
@@ -9,11 +10,11 @@ TWILIO_STATUS_PAGE_SIZE = 5
 
 def _get_notifications()->list:
     """
-    Returns a list of notifications not in final state, limited to 500
+    Returns a list of notifications not in final state
     """
-    query = select([Notification]).where(Notification.status != 'delivered').limit(TWILIO_STATUS_PAGE_SIZE)
+    # query = select([Notification]).where(Notification.status != 'delivered').limit(TWILIO_STATUS_PAGE_SIZE)
+    query = select([Notification]).where(Notification.status.in_(NOTIFICATION_STATUS_TYPES_COMPLETED)).limit(TWILIO_STATUS_PAGE_SIZE)
     return db.session.execute(query).all()
-
 
 
 @notify_celery.task(bind=True, name='update-twilio-status')
@@ -32,5 +33,3 @@ def update_twilio_status():
         if twilio_message:
             notification.status = twilio_message.status
             current_app.logger.info("Updated status for notification %s to %s", notification.id, notification.status)
-
-    return len(notifications)
