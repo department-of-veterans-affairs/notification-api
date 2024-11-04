d43513ddc6f805c7ddbe10c270ab518dfd3480cc
Michael Wellman <mchlwellman@gmail.com>
2024-11-04 05:56:25 -0500

    Adjusted logging

--- app/celery/twilio_tasks.py
+++ app/celery/twilio_tasks.py
@@ -33,9 +33,8 @@ def update_twilio_status():
     Update the status of notifications sent via Twilio
     """
     notifications = _get_notifications()
+    current_app.logger.info('Found %s notifications to update', len(notifications))
     for notification in notifications:
-        current_app.logger.info('Updating status for notification %s', notification.id)
-
         # The twilio message sid is in the 'reference' field
         message_sid = notification.reference
         twilio_sms_client.update_notification_status_override(message_sid)
