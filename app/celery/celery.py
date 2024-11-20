0da42e43993e7827f25daee96b54c893548151fb
Mackenzie Halliday <mackenzie.halliday@gmail.com>
2024-11-20 14:30:14 -0500

    Squash #Both patch celery for ddtrace in celery.py when celery instance intialized AND include ddtrace-run in command to initalize Celery ECS container

--- app/celery/celery.py
+++ app/celery/celery.py
@@ -1,8 +1,13 @@
 import time
 
 from celery import Celery, Task
+from ddtrace import patch
+
+patch(celery=True)
+
 from celery.signals import worker_process_shutdown, worker_shutting_down, worker_process_init
 from flask import current_app
+
 
 
 @worker_process_init.connect
