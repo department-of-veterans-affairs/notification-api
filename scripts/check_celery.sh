f7de27ec2105e39f8aa15b95e2d908cb20c83468
Mackenzie Halliday <mackenzie.halliday@gmail.com>
2024-11-21 15:04:46 -0500

    Squash #2041 - Update script to properly handle CELERY_CONCURRENCY as a varible

--- scripts/check_celery.sh
+++ scripts/check_celery.sh
@@ -5,9 +5,18 @@ set -e
 grep_string="run_celery.notify_celery\ worker"
 celery_pid_count=$(ps aux | grep -E run_celery.notify_celery\ worker | grep -v grep | wc -l)
 
-TOTAL_CELERY_PIDS=$((CELERY_CONCURRENCY + 1))
+echo "$CELERY_CONCURRENCY"
 
-if [ $celery_pid_count -ne TOTAL_CELERY_PIDS ]; then
+CELERY_CONCURRENCY_INT=$(echo "$CELERY_CONCURRENCY" | tr -d '[[:space:]]')
+
+if ! [ "$CELERY_CONCURRENCY_INT" -eq "$CELERY_CONCURRENCY_INT" ] 2>/dev/null; then
+  echo "CELERY_CONCURRENCY is not a valid integer: $CELERY_CONCURRENCY"
+  exit 1
+fi
+
+TOTAL_CELERY_PIDS=$((CELERY_CONCURRENCY_INT + 1))
+
+if [ $celery_pid_count -ne $TOTAL_CELERY_PIDS ]; then
   echo -e "There are an incorrect number of Celery PIDs: $celery_pid_count"
   exit 1
 else
