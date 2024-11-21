52de86ef95c5cff11bd00c5f39a8b2cbc4cef3ba
Mackenzie Halliday <mackenzie.halliday@gmail.com>
2024-11-21 13:58:44 -0500

    Squash 2041 - Try sending a variable for CELERY_CONCURRENCY for health check

--- scripts/check_celery.sh
+++ scripts/check_celery.sh
@@ -5,8 +5,9 @@ set -e
 grep_string="run_celery.notify_celery\ worker"
 celery_pid_count=$(ps aux | grep -E run_celery.notify_celery\ worker | grep -v grep | wc -l)
 
+TOTAL_CELERY_PIDS=$((CELERY_CONCURRENCY + 1))
 
-if [ $celery_pid_count -ne 17 ]; then
+if [ $celery_pid_count -ne TOTAL_CELERY_PIDS ]; then
   echo -e "There are an incorrect number of Celery PIDs: $celery_pid_count"
   exit 1
 else
