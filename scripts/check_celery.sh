a38dd0a5e3d081a58192de5cd5f9d6284d389d0e
Mackenzie Halliday <mackenzie.halliday@gmail.com>
2024-11-22 09:47:11 -0500

    Squash #2041 - Remove useless log

--- scripts/check_celery.sh
+++ scripts/check_celery.sh
@@ -7,11 +7,6 @@ celery_pid_count=$(ps aux | grep -E run_celery.notify_celery\ worker | grep -v g
 
 CELERY_CONCURRENCY_INT=$(echo "$CELERY_CONCURRENCY" | tr -d '[[:space:]]')
 
-if ! [ "$CELERY_CONCURRENCY_INT" -eq "$CELERY_CONCURRENCY_INT" ] 2>/dev/null; then
-  echo "CELERY_CONCURRENCY is not a valid integer: $CELERY_CONCURRENCY"
-  exit 1
-fi
-
 TOTAL_CELERY_PIDS=$((CELERY_CONCURRENCY_INT + 1))
 
 if [ $celery_pid_count -ne $TOTAL_CELERY_PIDS ]; then
