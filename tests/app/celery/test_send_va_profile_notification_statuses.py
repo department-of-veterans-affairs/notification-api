43cfdbef81890f63dddfff931d1921e2b1b516b1
Mackenzie Halliday <mackenzie.halliday@gmail.com>
2024-12-02 13:36:19 -0800

    Squash #2137 - testing pass for sms and email for new testing module

--- tests/app/celery/test_send_va_profile_notification_statuses.py
+++ tests/app/celery/test_send_va_profile_notification_statuses.py
@@ -1,10 +1,8 @@
+from datetime import datetime
 import os
-
-
 from unittest.mock import patch
-
 from app.celery import process_ses_receipts_tasks
-from app.constants import SMS_TYPE
+from app.constants import EMAIL_TYPE, SMS_TYPE
 from app.models import Notification
 
 
@@ -15,84 +13,84 @@ class TestSendNotificationStatusesToVAProfile:
         'to': '(732)846-6666',
         'status': 'delivered',
         'status_reason': '',
-        'created_at': '2024-07-25T10:00:00.0',
-        'completed_at': '2024-07-25T11:00:00.0',
-        'sent_at': '2024-07-25T11:00:00.0',
+        'created_at': datetime.fromisoformat('2024-07-25T10:00:00'),
+        'completed_at': datetime.fromisoformat('2024-07-25T11:00:00'),
+        'sent_at': datetime.fromisoformat('2024-07-25T11:00:00'),
         'notification_type': SMS_TYPE,
-        'provider': 'twilio',
+        'sent_by': 'twilio',
+    }
+
+    mock_email_notification_data = {
+        'id': '2e9e6920-4f6f-4cd5-9e16-fc306fe23867',
+        'reference': None,
+        'to': 'test@email.com',
+        'status': 'delivered',
+        'status_reason': '',
+        'created_at': datetime.fromisoformat('2024-07-25T10:00:00'),
+        'completed_at': datetime.fromisoformat('2024-07-25T11:00:00'),
+        'sent_at': datetime.fromisoformat('2024-07-25T11:00:00'),
+        'notification_type': EMAIL_TYPE,
+        'sent_by': 'ses',
     }
 
     @patch('app.va.va_profile.va_profile_client.requests.post')
     def test_can_send_sms_notification_status_to_va_profile(self, mock_post, notify_api, mocker):
-        mocker.patch.dict(os.environ, {'VA_PROFILE_EMAIL_STATUS_ENABLED': 'True'})
+        mocker.patch.dict(os.environ, {'VA_PROFILE_SMS_STATUS_ENABLED': 'True'})
 
-        sms_notification = Notification(
-            id=self.mock_sms_notification_data['id'],
-            reference=self.mock_sms_notification_data['reference'],
-            to=self.mock_sms_notification_data['to'],
-            status=self.mock_sms_notification_data['status'],
-            status_reason=self.mock_sms_notification_data['status_reason'],
-            created_at=self.mock_sms_notification_data['created_at'],
-            completed_at=self.mock_sms_notification_data['completed_at'],
-            sent_at=self.mock_sms_notification_data['sent_at'],
-            notification_type=self.mock_sms_notification_data['notification_type'],
-        )
+        sms_notification = Notification(**self.mock_sms_notification_data)
 
         process_ses_receipts_tasks.check_and_queue_va_profile_email_status_callback(sms_notification)
 
-        expected_url = 'http://mock.vaprofile.va.gov/contact-information-vanotify/notify/status'
+        va_profile_url = os.getenv('VA_PROFILE_URL')
+        expected_url = f'{va_profile_url}/contact-information-vanotify/notify/status'
+
         expected_payload = {
             'id': str(sms_notification.id),
-            'reference': sms_notification.client_reference,
+            'reference': sms_notification.reference,
             'to': sms_notification.to,
             'status': sms_notification.status,
             'status_reason': sms_notification.status_reason,
-            'created_at': sms_notification.created_at,
-            'completed_at': sms_notification.updated_at,
-            'sent_at': sms_notification.sent_at,
+            'created_at': sms_notification.created_at.isoformat() + '.000000Z',
+            'completed_at': None,
+            'sent_at': sms_notification.sent_at.isoformat() + '.000000Z',
             'notification_type': sms_notification.notification_type,
-            'provider': sms_notification.sent_by,
+            'provider': 'twilio',
         }
-        mock_post.assert_called_once_with(expected_url, json=expected_payload)
 
-    # def test_ut_check_and_queue_va_profile_email_status_callback_does_not_queue_task_if_feature_disabled(self, mocker):
-    #     mocker.patch('app.celery.process_ses_receipts_tasks.is_feature_enabled', return_value=False)
-    #     mock_send_email_status = mocker.patch(
-    #         'app.celery.process_ses_receipts_tasks.send_email_status_to_va_profile.apply_async'
-    #     )
-    #     mock_notification = mocker.patch('app.celery.process_ses_receipts_tasks.Notification')
+        expected_headers = {'Authorization': 'Bearer '}
+        expected_timeout = (30, 30)
 
-    #     process_ses_receipts_tasks.check_and_queue_va_profile_email_status_callback(mock_notification)
+        mock_post.assert_called_once_with(
+            expected_url, json=expected_payload, headers=expected_headers, timeout=expected_timeout
+        )
 
-    #     mock_send_email_status.assert_not_called()
+    @patch('app.va.va_profile.va_profile_client.requests.post')
+    def test_can_send_email_notification_status_to_va_profile(self, mock_post, notify_api, mocker):
+        mocker.patch.dict(os.environ, {'VA_PROFILE_SMS_STATUS_ENABLED': 'False'})
 
-    # def test_ut_check_and_queue_va_profile_email_status_callback_queues_task_if_feature_enabled(self, mocker):
-    #     mocker.patch('app.celery.process_ses_receipts_tasks.is_feature_enabled', return_value=True)
-    #     mock_send_email_status = mocker.patch(
-    #         'app.celery.process_ses_receipts_tasks.send_email_status_to_va_profile.apply_async'
-    #     )
-    #     mock_notification = mocker.patch('app.celery.process_ses_receipts_tasks.Notification')
+        email_notification = Notification(**self.mock_email_notification_data)
 
-    #     process_ses_receipts_tasks.check_and_queue_va_profile_email_status_callback(mock_notification)
+        process_ses_receipts_tasks.check_and_queue_va_profile_email_status_callback(email_notification)
 
-    #     mock_send_email_status.assert_called_once()
+        va_profile_url = os.getenv('VA_PROFILE_URL')
+        expected_url = f'{va_profile_url}/contact-information-vanotify/notify/status'
 
-    # def test_ut_send_email_status_to_va_profile(self, mocker):
-    #     mock_send_va_profile_email_status = mocker.patch(
-    #         'app.celery.process_ses_receipts_tasks.va_profile_client.send_va_profile_email_status'
-    #     )
+        expected_payload = {
+            'id': str(email_notification.id),
+            'reference': email_notification.reference,
+            'to': email_notification.to,
+            'status': email_notification.status,
+            'status_reason': email_notification.status_reason,
+            'created_at': email_notification.created_at.isoformat() + '.000000Z',
+            'completed_at': None,
+            'sent_at': email_notification.sent_at.isoformat() + '.000000Z',
+            'notification_type': email_notification.notification_type,
+            'provider': 'ses',
+        }
 
-    #     process_ses_receipts_tasks.send_email_status_to_va_profile(self.mock_notification_data)
+        expected_headers = {'Authorization': 'Bearer '}
+        expected_timeout = (30, 30)
 
-    #     mock_send_va_profile_email_status.assert_called_once_with(self.mock_notification_data)
-
-    # def test_ut_send_email_status_to_va_profile_raises_auto_retry_exception(self, mocker):
-    #     mock_send_va_profile_email_status = mocker.patch(
-    #         'app.celery.process_ses_receipts_tasks.va_profile_client.send_va_profile_email_status',
-    #         side_effect=[ConnectTimeout, ReadTimeout],
-    #     )
-
-    #     with pytest.raises(AutoRetryException):
-    #         process_ses_receipts_tasks.send_email_status_to_va_profile(self.mock_notification_data)
-
-    #     mock_send_va_profile_email_status.assert_called_once()
+        mock_post.assert_called_once_with(
+            expected_url, json=expected_payload, headers=expected_headers, timeout=expected_timeout
+        )
