2346fcb96ce268abb2426d54c9b925ee4214a1b3
Kyle MacMillan <16893311+k-macmillan@users.noreply.github.com>
2024-12-26 14:37:41 -0500

    Commented and formatted

--- app/celery/process_comp_and_pen.py
+++ app/celery/process_comp_and_pen.py
@@ -21,6 +21,7 @@ from app.va.identifier import IdentifierType
 def comp_and_pen_batch_process(records: list[dict[str, str]]) -> None:
     current_app.logger.debug(f'comp_and_pen_batch_process records: {records}')
 
+    # Grab all the necessary data
     try:
         service, template, sms_sender_id = lookup_notification_sms_setup_data(
             current_app.config['COMP_AND_PEN_SERVICE_ID'],
@@ -43,68 +44,64 @@ def comp_and_pen_batch_process(records: list[dict[str, str]]) -> None:
 
 
 def _send_comp_and_pen_sms(
-        service: Service,
-        template: Template,
-        sms_sender_id: str,
-        reply_to_text: str,
-        comp_and_pen_messages: list[DynamoRecord],
-        perf_to_number: str,
-    ) -> None:
-        """
-        Sends scheduled SMS notifications to recipients based on the provided parameters.
+    service: Service,
+    template: Template,
+    sms_sender_id: str,
+    reply_to_text: str,
+    comp_and_pen_messages: list[DynamoRecord],
+    perf_to_number: str,
+) -> None:
+    """
+    Sends scheduled SMS notifications to recipients based on the provided parameters.
 
-        Args:
-            :param service (Service): The service used to send the SMS notifications.
-            :param template (Template): The template used for the SMS notifications.
-            :param sms_sender_id (str): The ID of the SMS sender.
-            :param reply_to_text (str): The text a Veteran can reply to.
-            :param comp_and_pen_messages (list[DynamoRecord]): A list of DynamoRecord from the dynamodb table containing
-                the details needed to send the messages.
-            :param perf_to_number (str): The recipient's phone number.
+    Args:
+        :param service (Service): The service used to send the SMS notifications.
+        :param template (Template): The template used for the SMS notifications.
+        :param sms_sender_id (str): The ID of the SMS sender.
+        :param reply_to_text (str): The text a Veteran can reply to.
+        :param comp_and_pen_messages (list[DynamoRecord]): A list of DynamoRecord from the dynamodb table containing
+            the details needed to send the messages.
+        :param perf_to_number (str): The recipient's phone number.
 
-        Raises:
-            Exception: If there is an error while sending the SMS notification.
-        """
+    Raises:
+        Exception: If there is an error while sending the SMS notification.
+    """
 
+    for item in comp_and_pen_messages:
+        current_app.logger.debug('sending - record from dynamodb: %s', item.participant_id)
 
-        for item in comp_and_pen_messages:
-            current_app.logger.debug('sending - record from dynamodb: %s', item.participant_id)
+        # Use perf_to_number as the recipient if available. Otherwise, use vaprofile_id as recipient_item.
+        recipient_item = (
+            None
+            if perf_to_number is not None
+            else {
+                'id_type': IdentifierType.VA_PROFILE_ID.value,
+                'id_value': item.vaprofile_id,
+            }
+        )
 
-            # Use perf_to_number as the recipient if available. Otherwise, use vaprofile_id as recipient_item.
-            recipient = perf_to_number
-            recipient_item = (
-                None
-                if perf_to_number is not None
-                else {
-                    'id_type': IdentifierType.VA_PROFILE_ID.value,
-                    'id_value': item.vaprofile_id,
-                }
+        try:
+            # call generic method to send messages
+            send_notification_bypass_route(
+                service=service,
+                template=template,
+                reply_to_text=reply_to_text,
+                personalisation={'amount': item.payment_amount},
+                sms_sender_id=sms_sender_id,
+                recipient=perf_to_number,
+                recipient_item=recipient_item,
+                notification_id=uuid4(),
             )
-
-            try:
-                # call generic method to send messages
-                send_notification_bypass_route(
-                    service=service,
-                    template=template,
-                    reply_to_text=reply_to_text,
-                    personalisation={'amount': item.payment_amount},
-                    sms_sender_id=sms_sender_id,
-                    recipient=recipient,
-                    recipient_item=recipient_item,
-                    notification_id=uuid4(),
-                )
-            except Exception:
-                current_app.logger.exception(
-                    'Error attempting to send Comp and Pen notification with '
-                    'send_comp_and_pen_sms | record from dynamodb: %s',
-                    item.participant_id,
-                )
-            else:
-                if perf_to_number is not None:
-                    current_app.logger.info(
-                        'Notification sent using Perf simulated number %s instead of vaprofile_id', perf_to_number
-                    )
-
+        except Exception:
+            current_app.logger.exception(
+                'Error attempting to send Comp and Pen notification with '
+                'send_comp_and_pen_sms | record from dynamodb: %s',
+                item.participant_id,
+            )
+        else:
+            if perf_to_number is not None:
                 current_app.logger.info(
-                    'Notification sent to queue for record from dynamodb: %s', item.participant_id
+                    'Notification sent using Perf simulated number %s instead of vaprofile_id', perf_to_number
                 )
+
+            current_app.logger.info('Notification sent to queue for record from dynamodb: %s', item.participant_id)
--- app/integrations/comp_and_pen/scheduled_message_helpers.py
+++ app/integrations/comp_and_pen/scheduled_message_helpers.py
@@ -1,9 +1,6 @@
 from dataclasses import dataclass
-from time import monotonic
 from uuid import uuid4
 
-import boto3
-from boto3.dynamodb.conditions import Attr
 from flask import current_app
 from sqlalchemy.orm.exc import NoResultFound
 
@@ -36,72 +33,70 @@ class CompPenMsgHelper:
         self.dynamodb_table_name = dynamodb_table_name
 
     def batch_send_comp_and_pen_sms(
-            self,
-            service: Service,
-            template: Template,
-            sms_sender_id: str,
-            comp_and_pen_messages: list[DynamoRecord],
-            perf_to_number: str,
-        ) -> None:
-            """
-            Sends scheduled SMS notifications to recipients based on the provided parameters.
+        self,
+        service: Service,
+        template: Template,
+        sms_sender_id: str,
+        comp_and_pen_messages: list[DynamoRecord],
+        perf_to_number: str,
+    ) -> None:
+        """
+        Sends scheduled SMS notifications to recipients based on the provided parameters.
 
-            Args:
-                :param service (Service): The service used to send the SMS notifications.
-                :param template (Template): The template used for the SMS notifications.
-                :param sms_sender_id (str): The ID of the SMS sender.
-                :param comp_and_pen_messages (list[dict[str, int | float]]): A list of dictionaries from the dynamodb 
-                    table containing the details needed to send the messages.
-                :param perf_to_number (str): The recipient's phone number.
+        Args:
+            :param service (Service): The service used to send the SMS notifications.
+            :param template (Template): The template used for the SMS notifications.
+            :param sms_sender_id (str): The ID of the SMS sender.
+            :param comp_and_pen_messages (list[dict[str, int | float]]): A list of dictionaries from the dynamodb
+                table containing the details needed to send the messages.
+            :param perf_to_number (str): The recipient's phone number.
 
-            Raises:
-                Exception: If there is an error while sending the SMS notification.
-            """
+        Raises:
+            Exception: If there is an error while sending the SMS notification.
+        """
+        try:
+            reply_to_text = dao_get_service_sms_sender_by_id(service.id, sms_sender_id).sms_sender
+        except (NoResultFound, AttributeError):
+            current_app.logger.exception('Unable to send comp and pen notifications due to improper sms_sender')
+            raise
+
+        for item in comp_and_pen_messages:
+            current_app.logger.debug('sending - record from dynamodb: %s', item.participant_id)
+
+            # Use perf_to_number as the recipient if available. Otherwise, use vaprofile_id as recipient_item.
+            recipient = perf_to_number
+            recipient_item = (
+                None
+                if perf_to_number is not None
+                else {
+                    'id_type': IdentifierType.VA_PROFILE_ID.value,
+                    'id_value': item.vaprofile_id,
+                }
+            )
+
             try:
-                reply_to_text = dao_get_service_sms_sender_by_id(service.id, sms_sender_id).sms_sender
-            except (NoResultFound, AttributeError):
-                current_app.logger.exception('Unable to send comp and pen notifications due to improper sms_sender')
-                raise
-
-            for item in comp_and_pen_messages:
-                current_app.logger.debug('sending - record from dynamodb: %s', item.participant_id)
-
-                # Use perf_to_number as the recipient if available. Otherwise, use vaprofile_id as recipient_item.
-                recipient = perf_to_number
-                recipient_item = (
-                    None
-                    if perf_to_number is not None
-                    else {
-                        'id_type': IdentifierType.VA_PROFILE_ID.value,
-                        'id_value': item.vaprofile_id,
-                    }
+                # call generic method to send messages
+                send_notification_bypass_route(
+                    service=service,
+                    template=template,
+                    notification_type=SMS_TYPE,
+                    reply_to_text=reply_to_text,
+                    personalisation={'amount': item.payment_amount},
+                    sms_sender_id=sms_sender_id,
+                    recipient=recipient,
+                    recipient_item=recipient_item,
+                    notification_id=uuid4(),
                 )
-
-                try:
-                    # call generic method to send messages
-                    send_notification_bypass_route(
-                        service=service,
-                        template=template,
-                        notification_type=SMS_TYPE,
-                        reply_to_text=reply_to_text,
-                        personalisation={'amount': item.payment_amount},
-                        sms_sender_id=sms_sender_id,
-                        recipient=recipient,
-                        recipient_item=recipient_item,
-                        notification_id=uuid4(),
-                    )
-                except Exception:
-                    current_app.logger.exception(
-                        'Error attempting to send Comp and Pen notification with '
-                        'send_comp_and_pen_sms | record from dynamodb: %s',
-                        item.participant_id,
-                    )
-                else:
-                    if perf_to_number is not None:
-                        current_app.logger.info(
-                            'Notification sent using Perf simulated number %s instead of vaprofile_id', perf_to_number
-                        )
-
+            except Exception:
+                current_app.logger.exception(
+                    'Error attempting to send Comp and Pen notification with '
+                    'send_comp_and_pen_sms | record from dynamodb: %s',
+                    item.participant_id,
+                )
+            else:
+                if perf_to_number is not None:
                     current_app.logger.info(
-                        'Notification sent to queue for record from dynamodb: %s', item.participant_id
+                        'Notification sent using Perf simulated number %s instead of vaprofile_id', perf_to_number
                     )
+
+                current_app.logger.info('Notification sent to queue for record from dynamodb: %s', item.participant_id)
