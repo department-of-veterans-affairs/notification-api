153769f52fb237140eb0ff8cb31432b0a5b698f7
Kyle MacMillan <16893311+k-macmillan@users.noreply.github.com>
2025-01-07 11:17:40 -0500

    Added extra log parameter

--- app/clients/sms/twilio.py
+++ app/clients/sms/twilio.py
@@ -262,7 +262,11 @@ class TwilioSMSClient(SmsClient):
                 self.logger.debug('Twilio error details for %s - %s: %s', reference, e.code, e.msg)
                 raise NonRetryableException(status.status_reason) from e
             else:
-                self.logger.warning('Encountered a retryable error with sending an sms request to Twilio: %s', str(e))
+                self.logger.warning(
+                    'Notification: %s encountered a retryable error with sending an sms request to Twilio: %s',
+                    reference,
+                    str(e),
+                )
                 raise RetryableException(str(e)) from e
         except:
             self.logger.exception('Twilio send SMS request for %s failed', reference)
