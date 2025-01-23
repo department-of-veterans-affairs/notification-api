f3019ce43864b99f37214e8020ddeefe833eb94c
Kyle MacMillan <16893311+k-macmillan@users.noreply.github.com>
2025-01-22 21:27:38 -0500

    First pass

--- app/va/vetext/client.py
+++ app/va/vetext/client.py
@@ -1,11 +1,13 @@
-import requests
 from logging import Logger
-from typing import Dict
-from typing_extensions import TypedDict
 from time import monotonic
+from typing_extensions import TypedDict
+
+import requests
 from requests.auth import HTTPBasicAuth
 from notifications_utils.clients.statsd.statsd_client import StatsdClient
-from . import VETextRetryableException, VETextNonRetryableException, VETextBadRequestException
+
+from app.celery.exceptions import NonRetryableException, RetryableException
+from app.v2.notifications.dataclasses import V2PushPayload
 
 
 class Credentials(TypedDict):
@@ -29,72 +31,70 @@ class VETextClient:
         self.logger = logger
         self.statsd = statsd
 
+    @staticmethod
+    def format_for_vetext(payload: V2PushPayload) -> dict[str, str]:
+        if payload.personalisation:
+            payload.personalisation = {f'%{k.upper()}%': v for k, v in payload.personalisation.items()}
+        formatted_payload = {
+            'appSid': payload.app_sid,
+            'templateSid': payload.template_id,
+            'personalization': payload.personalisation,
+        }
+        if payload.is_broadcast():
+            formatted_payload['topicSid'] = payload.topic_sid
+        else:
+            formatted_payload['icn'] = payload.icn
+        return formatted_payload
+
     def send_push_notification(
         self,
-        mobile_app: str,
-        template_id: str,
-        identifier: str,
-        is_broadcast: bool = False,
-        personalization: Dict = None,
+        payload: dict[str, str],
     ) -> None:
-        """
-        If "is_broadcast" is True, "identifier" is the "topicSid".  Otherwise, it is the "icn".
-        """
+        """_summary_
 
-        formatted_personalization = None
-        if personalization:
-            formatted_personalization = {}
-            for key, value in personalization.items():
-                formatted_personalization[f'%{key.upper()}%'] = value
-
-        payload = {
-            'appSid': mobile_app,
-            ('topicSid' if is_broadcast else 'icn'): identifier,
-            'templateSid': template_id,
-            'personalization': formatted_personalization,
-        }
+        Args:
+            payload (dict[str, str]): _description_
+        """
         self.logger.debug('VEText Payload information: %s', payload)
-
+        start_time = monotonic()
         try:
-            start_time = monotonic()
             response = requests.post(
                 f'{self.base_url}/mobile/push/send', auth=self.auth, json=payload, timeout=self.TIMEOUT
             )
-            self.logger.info('VEText response: %s', response.json() if response.ok else response.status_code)
+            self.logger.info('VEText response: %s', response.json() if response.ok else response.text)
             response.raise_for_status()
         except requests.exceptions.ReadTimeout:
-            # Discussion with VEText: read timeouts are still processed, so are marking this as good
-            self.logger.exception('ReadTimeout raised sending push notification - Still processed, returning 201')
-            # Logging as error.read_timeout so we can easily track it
+            # Discussion with VEText: read timeouts are still processed, so no need to retry
+            self.logger.info('ReadTimeout raised sending push notification - notification still processed')
             self.statsd.incr(f'{self.STATSD_KEY}.error.read_timeout')
         except requests.exceptions.ConnectTimeout as e:
-            self.logger.exception('ConnectTimeout raised sending push notification - Retrying')
-            # Logging as error.read_timeout so we can easily track it
+            self.logger.warning('ConnectTimeout raised sending push notification - Retrying')
             self.statsd.incr(f'{self.STATSD_KEY}.error.connection_timeout')
-            raise VETextRetryableException from e
+            raise RetryableException from e
         except requests.HTTPError as e:
             self.logger.exception('HTTPError raised sending push notification')
             self.statsd.incr(f'{self.STATSD_KEY}.error.{e.response.status_code}')
             if e.response.status_code in [429, 500, 502, 503, 504]:
                 self.logger.warning('Retryable exception raised with status code: %s', e.response.status_code)
-                raise VETextRetryableException from e
+                raise RetryableException from e
             elif e.response.status_code == 400:
                 self._decode_bad_request_response(e)
             else:
-                self.logger.critical(
+                payload['icn'] = '<redacted>'
+                self.logger.exception(
                     'Status: %s - Not retrying - payload: %s',
                     e.response.status_code,
                     payload,
                 )
-                raise VETextNonRetryableException from e
+                raise NonRetryableException from e
         except requests.RequestException as e:
-            self.logger.critical(
+            payload['icn'] = '<redacted>'
+            self.logger.exception(
                 'Exception raised sending push notification. Not retrying - payload: %s',
                 payload,
             )
-            self.logger.exception(e)
             self.statsd.incr(f'{self.STATSD_KEY}.error.request_exception')
-            raise VETextNonRetryableException from e
+            raise NonRetryableException from e
         else:
             self.statsd.incr(f'{self.STATSD_KEY}.success')
         finally:
@@ -109,8 +109,9 @@ class VETextClient:
             payload = http_exception.response.json()
         except Exception:
             message = http_exception.response.text
-            raise VETextBadRequestException(message=message) from http_exception
+            self.logger.warning('Bad response from VEText: %s', message)
+            raise NonRetryableException(message=message) from http_exception
         else:
             field = payload.get('idType')
             message = payload.get('error')
-            raise VETextBadRequestException(field=field, message=message) from http_exception
+            raise NonRetryableException(field=field, message=message) from http_exception
