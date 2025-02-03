bf72178d9287a67b8451b6c56878c7e4ad0e86d4
Mackenzie Halliday <mackenzie.halliday@gmail.com>
2025-02-03 09:56:57 -0500

    2172 - Fix to refactor, split out testing after refactor to validation method

--- tests/app/integration/test_push_notification.py
+++ tests/app/integration/test_push_notification.py
@@ -54,20 +54,8 @@ def test_mobile_app_push_notification_delivered(
     assert response.status_code == 201
 
 
-@pytest.mark.parametrize(
-    'function, app',
-    [
-        ('validate', None),
-        ('mobile_app_registry.get_app', 'mobile_app'),
-        ('mobile_app_registry.get_app', None),
-    ],
-)
-@pytest.mark.parametrize('test_exception', [KeyError, TypeError, ValidationError])
-def test_mobile_app_push_notification_failed_validation(
+def test_push_notification_validation_fails(
     client,
-    test_exception,
-    function,
-    app,
     rmock,
     mocker,
     sample_api_key,
@@ -84,13 +72,8 @@ def test_mobile_app_push_notification_failed_validation(
         'recipient_identifier': {'id_type': 'ICN', 'id_value': 'some-icn'},
         'personalisation': {'%FOO%': 'bar'},
     }
-    # Test mobile_app there and not there cases
-    if app:
-        push_request_body['mobile_app'] = 'VETEXT'
-        mocker.patch.dict(os.environ, {'VETEXT_SID': '1234', 'VA_FLAGSHIP_APP_SID': '1234'})
 
-    # Raise the appropriate exception at the correct function
-    mocker.patch(f'app.v2.notifications.rest_push.{function}', side_effect=test_exception)
+    mocker.patch('app.v2.notifications.rest_push.validate', side_effect=ValidationError('Validation failed'))
 
     response = client.post(
         url_for('v2_notifications.send_push_notification', service_id=service.id),
@@ -101,7 +84,55 @@ def test_mobile_app_push_notification_failed_validation(
         ],
     )
 
-    # These are all BadRequestError and should be visible in the response
+    assert 'BadRequestError' in str(response.json)
+    assert response.status_code == 400
+
+
+@pytest.mark.parametrize(
+    'mobile_app, test_exception',
+    [
+        ('VETEXT', KeyError),
+        ('VETEXT', ValidationError),
+        (None, KeyError),
+        (None, ValidationError),
+    ],
+)
+def test_push_notification_app_registry_lookup_fails(
+    client,
+    mobile_app,
+    test_exception,
+    rmock,
+    mocker,
+    sample_api_key,
+    sample_service,
+):
+    service = sample_service(service_permissions=[PUSH_TYPE])
+    api_key = sample_api_key(service=service)
+    rmock.register_uri(
+        'POST', f"{client.application.config['VETEXT_URL']}/mobile/push/send", json={'result': 'success'}
+    )
+
+    push_request_body = {
+        'template_id': 'some-template-id',
+        'recipient_identifier': {'id_type': 'ICN', 'id_value': 'some-icn'},
+        'personalisation': {'%FOO%': 'bar'},
+    }
+
+    if mobile_app:
+        push_request_body['mobile_app'] = mobile_app
+        mocker.patch.dict(os.environ, {'VETEXT_SID': '1234', 'VA_FLAGSHIP_APP_SID': '1234'})
+
+    mocker.patch('app.v2.notifications.rest_push.mobile_app_registry.get_app', side_effect=test_exception)
+
+    response = client.post(
+        url_for('v2_notifications.send_push_notification', service_id=service.id),
+        data=json.dumps(push_request_body),
+        headers=[
+            ('Content-Type', 'application/json'),
+            create_authorization_header(api_key),
+        ],
+    )
+
     assert 'BadRequestError' in str(response.json)
     assert response.status_code == 400
 
