d1fe86ef1fb8417bc2342e90a99a5b1c1f27452d
EvanParish <evan.parish@oddball.io>
2025-02-07 09:49:46 -0600

    Add revoked field to api keys

---
+++ migrations/versions/0375_add_key_revoked.py
@@ -0,0 +1,27 @@
+"""
+
+Revision ID: 0375_add_key_revoked
+Revises: 0374_add_expected_cadence
+Create Date: 2025-02-06 21:50:38.351026
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
+
+revision = '0375_add_key_revoked'
+down_revision = '0374_add_expected_cadence'
+
+
+def upgrade():
+    op.add_column('api_keys', sa.Column('revoked', sa.Boolean(), nullable=False))
+    op.drop_index('uix_service_to_key_name', table_name='api_keys', postgresql_where='(expiry_date IS NULL)')
+    op.create_index('uix_service_and_key_name', 'api_keys', ['service_id', 'name'], unique=True, postgresql_where=sa.text('revoked IS false'))
+    op.add_column('api_keys_history', sa.Column('revoked', sa.Boolean(), nullable=False))
+
+
+def downgrade():
+    op.drop_column('api_keys_history', 'revoked')
+    op.drop_index('uix_service_and_key_name', table_name='api_keys', postgresql_where=sa.text('revoked IS false'))
+    op.create_index('uix_service_to_key_name', 'api_keys', ['service_id', 'name'], unique=True, postgresql_where='(expiry_date IS NULL)')
+    op.drop_column('api_keys', 'revoked')
