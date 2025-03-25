1b91a94f45dff1b08ad8f35387b14b1abaafdbbe
Kyle MacMillan <16893311+k-macmillan@users.noreply.github.com>
2025-03-25 06:15:53 -0400

    Added issue number

--- tests/app/dao/test_communication_item_dao.py
+++ tests/app/dao/test_communication_item_dao.py
@@ -1,17 +1,4 @@
-import pytest
-from sqlalchemy import delete
-
-from app.models import CommunicationItem
-
-
-@pytest.fixture
-def db_session_with_empty_communication_items(notify_db_session):
-    stmt = delete(CommunicationItem)
-    notify_db_session.session.execute(stmt)
-    notify_db_session.session.commit()
-    return notify_db_session
-
 
 class TestGetCommunicationItems:
-    # Need test(s) for get_communication_item
-    pass
+    # Issue #2371
+    ...
