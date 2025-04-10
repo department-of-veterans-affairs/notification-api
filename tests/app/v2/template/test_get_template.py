4089574e692e544a9c5d64befeacc0111c2da9c9
Michael Wellman <mchlwellman@gmail.com>
2025-04-10 09:06:06 -0400

    Mocked new feature flag where needed

--- tests/app/v2/template/test_get_template.py
+++ tests/app/v2/template/test_get_template.py
@@ -20,7 +20,11 @@ def test_get_template_by_id_returns_200(
     tmp_type,
     version,
     sample_template,
+    mocker,
 ):
+    # Mock the feature flag to return True
+    mocker.patch('app.dao.templates_dao.is_feature_enabled', return_value=True)
+
     api_key = sample_api_key()
     template = sample_template(service=api_key.service, template_type=tmp_type)
     auth_header = create_authorization_header(api_key)
