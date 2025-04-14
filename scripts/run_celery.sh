a3a55e1b3f52afd0d81b702d59a156e7030c7192
Kyle MacMillan <16893311+k-macmillan@users.noreply.github.com>
2025-04-14 15:14:30 -0400

    Updated worker commands

--- scripts/run_celery.sh
+++ scripts/run_celery.sh
@@ -3,5 +3,7 @@
 set -e
 
 # Necessary to run as exec so the PID is transferred to Celery for the `SIGTERM` sent from ECS
-ddtrace-run celery -A run_celery.notify_celery worker -n priority --pidfile="/tmp/celery_priority.pid" --loglevel=INFO --concurrency=$CELERY_CONCURRENCY -Q send-sms-tasks, send-email-tasks, lookup-contact-info-tasks, lookup-va-profile-id-tasks
+
+ddtrace-run celery -A run_celery.notify_celery worker -n priority --pidfile="/tmp/celery_priority.pid" --loglevel=INFO --concurrency=$CELERY_CONCURRENCY -Q send-sms-tasks,send-email-tasks,lookup-contact-info-tasks,lookup-va-profile-id-tasks &
 ddtrace-run celery -A run_celery.notify_celery worker -n all --pidfile="/tmp/celery_all.pid" --loglevel=INFO --concurrency=$CELERY_CONCURRENCY
+
