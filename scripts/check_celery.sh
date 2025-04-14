23c3506c6d32533ef0cfcbe1d9c4c928a335c225
Kyle MacMillan <16893311+k-macmillan@users.noreply.github.com>
2025-04-14 14:20:41 -0400

    Updated workers

--- scripts/check_celery.sh
+++ scripts/check_celery.sh
@@ -7,7 +7,8 @@ celery_pid_count=$(ps aux | grep -E run_celery.notify_celery\ worker | grep -v g
 
 CELERY_CONCURRENCY_INT=$(echo "$CELERY_CONCURRENCY" | tr -d '[[:space:]]')
 
-TOTAL_CELERY_PIDS=$((CELERY_CONCURRENCY_INT + 1))
+# 2 workers
+TOTAL_CELERY_PIDS=$((CELERY_CONCURRENCY_INT + 2))
 
 if [ $celery_pid_count -ne $TOTAL_CELERY_PIDS ]; then
   echo -e "There are an incorrect number of Celery PIDs: $celery_pid_count"
