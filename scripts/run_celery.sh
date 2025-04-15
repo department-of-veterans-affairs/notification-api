e39f3ce4520c2c496acff602baa85de923a36165
Kyle MacMillan <16893311+k-macmillan@users.noreply.github.com>
2025-04-15 13:12:51 -0400

    Added clarity/comments

--- scripts/run_celery.sh
+++ scripts/run_celery.sh
@@ -5,8 +5,8 @@ set -e
 PRIO_PID_LOG=/tmp/celery_priority.pid
 ALL_PID_LOG=/tmp/celery_all.pid
 
-post_execution_handler() {
-  # Post Execution
+# Triggers when a SIGTERM is caught (trap command below)
+sigterm_handler() {
   echo "Start gracefull worker shutdown..."
   # Gracefully shutdown workers
   kill -TERM "$WORKER_PRIO_PID"
@@ -16,26 +16,30 @@ post_execution_handler() {
   wait "$WORKER_PRIO_PID" && rm -f $PRIO_PID_LOG
   wait "$WORKER_ALL_PID" && rm -f $ALL_PID_LOG
   echo "...Workers shut down gracefully"
-  exit 143; # 128 + 15 -- SIGTERM
+  # 128 + 15 -- SIGTERM
+  exit 143
 }
 
-# Look for SIGTERM
-trap post_execution_handler SIGTERM
+# Execute the sigterm_handler function if this PID receives a SIGTERM
+trap sigterm_handler SIGTERM
 
-echo "Start the Priority queue worker";
+echo "Start the Priority queue worker"
 # rm in case of manual shutdown (local)
 rm -f $PRIO_PID_LOG
+# Run the worker async and capture the PID it was assigned
 ddtrace-run celery -A run_celery.notify_celery worker -n priority --pidfile="$PRIO_PID_LOG" --loglevel=INFO --concurrency=$CELERY_CONCURRENCY -Q send-sms-tasks,send-email-tasks,lookup-contact-info-tasks,lookup-va-profile-id-tasks &
 WORKER_PRIO_PID=$!
 echo "Worker: Priority PID: $WORKER_PRIO_PID"
 
-echo "Start the All queue worker";
+echo "Start the All queue worker"
 # rm in case of manual shutdown (local)
 rm -f $ALL_PID_LOG
+# Run the worker async and capture the PID it was assigned
 ddtrace-run celery -A run_celery.notify_celery worker -n all --pidfile="$ALL_PID_LOG" --loglevel=INFO --concurrency=$CELERY_CONCURRENCY &
 WORKER_ALL_PID=$!
 echo "Worker: All PID: $WORKER_ALL_PID"
 
-# Script/container runs as long as these two are running
+# The script waits as long as these two are running. 
+# Normal use case would terminate this script in the sigterm_handler, not after these two
 wait "$WORKER_PRIO_PID"
 wait "$WORKER_ALL_PID"
