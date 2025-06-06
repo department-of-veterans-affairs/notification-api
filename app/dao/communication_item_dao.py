5110afe346a3ce2755f2d2ed2e94f1acb85cc689
David J. Kalbfleisch <1.21e9W@protonmail.com>
2025-06-06 15:06:58 -0400

    #2371 Communication item dao unit testing

--- app/dao/communication_item_dao.py
+++ app/dao/communication_item_dao.py
@@ -7,14 +7,5 @@ from app import db
 from app.models import CommunicationItem
 
 
-def dao_create_communication_item(communication_item: CommunicationItem):
-    communication_item.id = communication_item.id if communication_item.id else uuid.uuid4()
-    db.session.add(communication_item)
-
-
-def get_communication_items() -> List[CommunicationItem]:
-    return db.session.scalars(select(CommunicationItem)).all()
-
-
 def get_communication_item(communication_item_id) -> CommunicationItem:
     return db.session.scalars(select(CommunicationItem).where(CommunicationItem.id == communication_item_id)).one()
