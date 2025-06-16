f08dcc1b4cc5f53e24d0dd25a50db6465899913b
Mackenzie Halliday <mackenzie.halliday@gmail.com>
2025-06-16 11:07:06 -0700

    #2355 - Initial commit

--- app/schemas.py
+++ app/schemas.py
@@ -522,7 +522,6 @@ class NotificationModelSchema(BaseSchema):
 
 class BaseTemplateSchema(BaseSchema):
     reply_to = fields.Method('get_reply_to', allow_none=True, deserialize='set_reply_to')
-    reply_to_text = fields.Method('get_reply_to_text', allow_none=True, deserialize='set_reply_to')
 
     def set_reply_to(self, obj):
         return str(obj)
