2695bd80cc5763b7ac8956edd423b118a8592b0f
David Kalbfleisch <1.21e9W@protonmail.com>
2025-07-10 15:24:21 -0400

    squash 2519

---
+++ tests/app/pii/__init__.py
@@ -0,0 +1,21 @@
+"""PII handling package.
+
+This package provides classes and utilities for handling Personally Identifiable Information (PII)
+in a secure manner, including encryption, redaction, and controlled access.
+"""
+
+from app.pii.pii_base import PiiEncryption, PiiLevel, Pii
+from app.pii.pii_high import PiiBirlsid, PiiEdipi, PiiIcn
+from app.pii.pii_high import PiiPid, PiiVaProfileID
+
+
+__all__ = [
+    'Pii',
+    'PiiBirlsid',
+    'PiiEdipi',
+    'PiiEncryption',
+    'PiiIcn',
+    'PiiLevel',
+    'PiiPid',
+    'PiiVaProfileID',
+]
