5ad1d4e79144b5e47948c001d5923abc4c8b020a
David J. Kalbfleisch <1.21e9W@protonmail.com>
2025-07-28 11:41:39 -0400

    1936 - remove feature flags

--- app/config.py
+++ app/config.py
@@ -100,8 +100,6 @@ class Config(object):
     # URL of redis instance
     REDIS_URL = os.getenv('REDIS_URL')
     REDIS_ENABLED = os.getenv('REDIS_ENABLED') == 'True'
-    API_RATE_LIMIT_ENABLED = os.getenv('API_RATE_LIMIT_ENABLED', 'False') == 'True'
-    API_MESSAGE_LIMIT_ENABLED = os.getenv('API_MESSAGE_LIMIT_ENABLED', 'False') == 'True'
     EXPIRE_CACHE_TEN_MINUTES = 600
     EXPIRE_CACHE_EIGHT_DAYS = 8 * 24 * 60 * 60
 
@@ -428,9 +426,7 @@ class Config(object):
     SESSION_COOKIE_SAMESITE = 'Lax'
 
     # Google Analytics
-    GOOGLE_ANALYTICS_ENABLED = str(True) == (os.getenv('GOOGLE_ANALYTICS_ENABLED', 'False'))
     GOOGLE_ANALYTICS_URL = os.getenv('GOOGLE_ANALYTICS_URL', 'https://www.google-analytics.com/collect')
-    GOOGLE_ANALYTICS_TID = os.getenv('GOOGLE_ANALYTICS_TID', 'UA-50123418-17')
     GA4_URL = os.getenv('GA4_URL', 'https://www.google-analytics.com/mp/collect')
     GA4_MEASUREMENT_ID = os.getenv('GA4_MEASUREMENT_ID', '')
     GA4_API_SECRET = os.getenv('GA4_API_SECRET', '')
@@ -527,8 +523,6 @@ class Test(Development):
 
     TWILIO_INBOUND_SMS_USERNAMES = '["username"]'
     TWILIO_INBOUND_SMS_PASSWORDS = '["password"]'
-
-    GOOGLE_ANALYTICS_ENABLED = True
 
     AWS_REGION = 'us-gov-west-1'
     AWS_SES_EMAIL_FROM_DOMAIN = 'test domain'
