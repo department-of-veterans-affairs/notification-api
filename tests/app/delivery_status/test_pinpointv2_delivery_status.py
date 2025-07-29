e15caeb4fa8e92085e384a669ee475b7073efd9e
Chris Johnson <christopher.johnson@oddball.io>
2025-07-29 15:12:50 -0400

    update test log mocking

--- tests/app/delivery_status/test_pinpointv2_delivery_status.py
+++ tests/app/delivery_status/test_pinpointv2_delivery_status.py
@@ -11,7 +11,7 @@ from flask import url_for
     ],
 )
 def test_post_delivery_status(client, mocker, query_string):
-    mock_logger = mocker.patch('app.internal.rest.current_app.logger.info')
+    mock_logger = mocker.patch('app.delivery_status.rest.current_app.logger.info')
 
     response = client.post(url_for('pinpoint_v2.handler', **query_string[0]), json={'key': 'value'})
 
