efb1684d45a1a34d041e609484ae875ce7fa0a1b
Chris Johnson <christopher.johnson@oddball.io>
2025-08-04 13:37:40 -0400

    update endpoint unit test with auth

--- tests/app/delivery_status/test_pinpointv2_delivery_status.py
+++ tests/app/delivery_status/test_pinpointv2_delivery_status.py
@@ -1,19 +1,27 @@
-# @pytest.mark.parametrize(
-#     'post_json',
-#     [
-#         {},
-#         {'foo': 'bar'},
-#         {'foo': 'bar', 'baz': 'qux'},
-#     ],
-# )
-# def test_post_delivery_status(client, mocker, post_json):
-#     mock_logger = mocker.patch('app.delivery_status.rest.current_app.logger.info')
+from flask import url_for
+import pytest
 
-#     response = client.post(url_for('pinpoint_v2.handler'), json=post_json)
 
-#     assert response.status_code == 200
-#     assert response.json == {'status': 'received'}
+@pytest.mark.parametrize(
+    'post_json',
+    [
+        {},
+        {'foo': 'bar'},
+        {'foo': 'bar', 'baz': 'qux'},
+    ],
+)
+def test_post_delivery_status(client, mocker, post_json):
+    mock_logger = mocker.patch('app.delivery_status.rest.current_app.logger.info')
 
-#     actual = mock_logger.call_args_list[0].args[0]
-#     expected = 'PinpointV2 delivery-status request: %s'
-#     assert actual == expected, 'The logger was not called with the expected message.'
+    response = client.post(
+        url_for('pinpoint_v2.handler'),
+        json=post_json,
+        headers=[('X-Amz-Firehose-Access-Key', 'dev')],
+    )
+
+    assert response.status_code == 200
+    assert response.json == {'status': 'received'}
+
+    actual = mock_logger.call_args_list[0].args[0]
+    expected = 'PinpointV2 delivery-status request: %s'
+    assert actual == expected, 'The logger was not called with the expected message.'
