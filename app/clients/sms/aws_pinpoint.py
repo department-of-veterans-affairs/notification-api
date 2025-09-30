e6d7047d37e6eae67e8bdbb20d6a68e77a4811d9
Mackenzie Halliday <mackenzie.halliday@gmail.com>
2025-09-30 13:59:24 -0700

    HOTFIX

--- app/clients/sms/aws_pinpoint.py
+++ app/clients/sms/aws_pinpoint.py
@@ -130,13 +130,24 @@ class AwsPinpointClient(SmsClient):
         except (botocore.exceptions.ClientError, Exception) as e:
             self.statsd_client.incr('clients.pinpoint.error')
             msg = str(e)
+            error_code = e.response.get('Error', {}).get('Code', '')
 
-            # Check for opt-out conflict (only for ClientError)
-            if isinstance(e, botocore.exceptions.ClientError):
-                error_code = e.response.get('Error', {}).get('Code', '')
-                if error_code == 'ConflictException' and 'DESTINATION_PHONE_NUMBER_OPTED_OUT' in msg:
-                    self.logger.warning('Phone number has opted out: %s', recipient_number)
-                    raise NonRetryableException(f'Phone number has opted out: {recipient_number}')
+            if error_code == 'ConflictException':
+                reason = e.response.get('Reason', 'UNKNOWN')
+                resource_type = e.response.get('ResourceType', 'unknown')
+                resource_id = e.response.get('ResourceId', 'unknown')
+
+                self.logger.warning(
+                    'ConflictException sending SMS - Reason: %s, ResourceType: %s, ResourceId: %s, Recipient: %s',
+                    reason,
+                    resource_type,
+                    resource_id,
+                    recipient_number,
+                )
+                self.statsd_client.incr(f'{SMS_TYPE}.{PINPOINT_PROVIDER}_request.conflict.{reason.lower()}')
+                raise NonRetryableException(
+                    f'ConflictException: {reason} - ResourceType: {resource_type}, ResourceId: {resource_id}'
+                )
 
             if any(code in msg for code in AwsPinpointClient._retryable_v1_codes):
                 self.logger.warning('Encountered a Retryable exception: %s - %s', type(e).__class__.__name__, msg)
