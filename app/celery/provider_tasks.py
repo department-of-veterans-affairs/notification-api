6f3a7d9ea09e6aa934b820c93e73f7ca340615e2
Mackenzie Halliday <mack.halliday@users.noreply.github.com>
2026-01-27 22:05:03 -0700

    #2550 - Add logic to increase retry_count by 1 in deliver_sms and deliver_email celery tasks, add testing

--- app/celery/provider_tasks.py
+++ app/celery/provider_tasks.py
@@ -56,6 +56,14 @@ def deliver_sms(
     notification_id,
     sms_sender_id=None,
 ):
+    if task.request.retries > 0:
+        db_retry_count = notifications_dao.dao_increment_notification_retry_count(notification_id)
+        current_app.logger.info(
+            'deliver_sms retry attempt for notification %s, total retry_count now: %s',
+            notification_id,
+            db_retry_count,
+        )
+
     current_app.logger.info(
         'Start sending SMS for notification id: %s', notification_id, extra={'sms_sender_id': sms_sender_id}
     )
@@ -167,6 +175,14 @@ def deliver_email(
     notification_id: UUID,
     sms_sender_id: UUID = None,
 ):
+    if task.request.retries > 0:
+        db_retry_count = notifications_dao.dao_increment_notification_retry_count(notification_id)
+        current_app.logger.info(
+            'deliver_email retry attempt for notification %s, total retry_count now: %s',
+            notification_id,
+            db_retry_count,
+        )
+
     current_app.logger.info('Start sending email for notification id: %s', notification_id)
 
     try:
