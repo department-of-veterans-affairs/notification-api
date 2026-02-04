f583e0f483e699b79e03bcf96349f96f9307a66c
Derek Line <derek.r.line@gmail.com>
2026-02-04 12:59:20 -0500

    Update 0382_add_cache_encryption.py

--- migrations/versions/0382_add_cache_encryption.py
+++ migrations/versions/0382_add_cache_encryption.py
@@ -15,8 +15,11 @@ down_revision = '0381_add_retry_fields'
 def upgrade():
     op.add_column('va_profile_local_cache', sa.Column('encrypted_va_profile_id', sa.Text(), nullable=True))
     op.add_column('va_profile_local_cache', sa.Column('encrypted_participant_id', sa.Text(), nullable=True))
+    op.create_unique_constraint('uix_veteran_id', 'va_profile_local_cache', ['va_profile_id', 'encrypted_va_profile_id', 'communication_item_id', 'communication_channel_id'])
 
 
 def downgrade():
     op.drop_column('va_profile_local_cache', 'encrypted_participant_id')
     op.drop_column('va_profile_local_cache', 'encrypted_va_profile_id')
+    op.drop_constraint('uix_veteran_id', 'va_profile_local_cache', type_='unique')
+    op.create_unique_constraint(op.f('uix_veteran_id'), 'va_profile_local_cache', ['va_profile_id', 'communication_item_id', 'communication_channel_id'])
