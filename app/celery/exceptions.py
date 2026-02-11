a410ae3449e447235289bbf2ce5a1b4c600b5ca8
Chris Johnson <christopher.johnson@oddball.io>
2026-02-11 09:11:52 -0500

    add comment to exception

--- app/celery/exceptions.py
+++ app/celery/exceptions.py
@@ -1,4 +1,11 @@
 class RetryableException(Exception):
+    """
+    Indicates a transient failure that should be retried.
+
+    The ``use_non_priority_handling`` flag signals that retry handling should
+    override the default behavior (e.g., route to a non-priority queue).
+    """
+
     def __init__(self, message='', *, use_non_priority_handling=False):
         super().__init__(message)
         self.use_non_priority_handling = use_non_priority_handling
