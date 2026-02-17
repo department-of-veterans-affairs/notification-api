0ee9513e2a6b016c4a5b33e513437439c53af9f9
Derek Line <67062146+derekline@users.noreply.github.com>
2026-02-17 16:38:31 -0500

    TEAM-1956 - FUNCTIONAL: Support encrypted and unencrypted PII in VA Profile Opt In Out Lambda (#2698)

--- tests/lambda_functions/conftest.py
+++ tests/lambda_functions/conftest.py
@@ -2,12 +2,36 @@ from random import randint
 
 import pytest
 from sqlalchemy import delete
+import base64
 
 from app.models import VAProfileLocalCache
+from lambda_functions.va_profile.va_profile_opt_in_out_lambda import EncryptedVAProfileId
+
+
+# Helpers
+def generate_base64_test_key(value: str) -> str:
+    """Generates the base64 encoding of the input string value.
+
+    Note: This generates constant test key(s) for consistent encryption/decryption during tests
+    Using a fixed test key - this is only for testing and not a real secret
+    """
+    return base64.b64encode(value.encode('utf-8')).decode('utf-8')
+
+
+# PII encryption test setup
+TEST_ENCRYPTION_KEY = generate_base64_test_key('This is an 32 byte key for tests')
+TEST_HMAC_KEY = generate_base64_test_key('This is a second 32 byte key for HMAC tests')
 
 
 @pytest.fixture
-def sample_va_profile_local_cache(notify_db_session):
+def mock_pii_env_vars(monkeypatch):
+    monkeypatch.setenv('PII_ENCRYPTION_KEY_PATH', '/fake/path/to/pii_encryption_key')
+    monkeypatch.setenv('PII_ENCRYPTION_KEY', TEST_ENCRYPTION_KEY)
+    monkeypatch.setenv('PII_HMAC_KEY', TEST_HMAC_KEY)
+
+
+@pytest.fixture
+def sample_va_profile_local_cache(notify_db_session, mock_pii_env_vars):
     created_va_profile_local_cache_ids = []
 
     def _sample_va_profile_local_cache(
@@ -20,10 +44,14 @@ def sample_va_profile_local_cache(notify_db_session):
         """
         The combination of va_profile_id, communication_item_id, and communication_channel_id must be unique.
         """
+        va_profile_id: int = va_profile_id if va_profile_id else randint(1000, 100000)
+        encrypted_va_profile_id: EncryptedVAProfileId = EncryptedVAProfileId(va_profile_id)
 
         va_profile_local_cache = VAProfileLocalCache(
             allowed=allowed,
-            va_profile_id=(va_profile_id if (va_profile_id is not None) else randint(1000, 100000)),
+            va_profile_id=encrypted_va_profile_id.get_pii(),
+            encrypted_va_profile_id=encrypted_va_profile_id.fernet_encryption,
+            encrypted_va_profile_id_blind_index=encrypted_va_profile_id.hmac_encryption,
             communication_item_id=communication_item_id,
             communication_channel_id=communication_channel_id,
             source_datetime=source_datetime,
